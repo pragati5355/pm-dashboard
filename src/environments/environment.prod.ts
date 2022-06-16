92ee295a8c1cba0bfa5e99c09a1fc894d0cb15c1
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2022-06-16 22:59:05 +0530

    remove status form profile popup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://13f5-103-127-166-119.in.ngrok.io/login/oauth2/code/google',
+  baseUrl: 'https://dljmungqb9yx1.cloudfront.net/',
   GoogleClientId: "348103697312-ibthsgsun4vi3mcpa6i078f513ed6sq3.apps.googleusercontent.com"
 };
