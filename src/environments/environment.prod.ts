80a9c23cae4c372e9b8ce78b27382e56a31108b1
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2022-06-17 12:41:35 +0530

    google id change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://11vi8vyl44.execute-api.ap-south-1.amazonaws.com/',
-  GoogleClientId: "348103697312-ibthsgsun4vi3mcpa6i078f513ed6sq3.apps.googleusercontent.com"
+  GoogleClientId: "838887289941-io1mlmn22eepf15r99lj671g6ok50c6r.apps.googleusercontent.com"
 };
