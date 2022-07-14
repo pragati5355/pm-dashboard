1ea2127498d04416d7d02d7ca1ade6d440dc699a
Naynesh Rathod <naynesh.rathod@mindbowser.com>
2022-07-14 19:49:05 +0530

    - add team list resource team list

--- src/app/core/constacts/static.ts
+++ src/app/core/constacts/static.ts
@@ -1,42 +1,79 @@
+export const StaticData = {
+  ROLE_LIST: [
+    {value: 'Android', viewValue: 'Android'},
+    {value: 'Backend Dev', viewValue: 'Backend Dev'},
+    {value: 'Data Science', viewValue: 'Data Science'},
+    {value: 'Designer', viewValue: 'Designer'},
+    {value: 'DevOps', viewValue: 'DevOps'},
+    {value: 'Dot Net', viewValue: 'Dot Net'},
+    {value: 'Frontend Dev', viewValue: 'Frontend Dev'},
+    {value: 'Full Stack (Java + Angular)', viewValue: 'Full Stack (Java + Angular)'},
+    {value: 'Full Stack (Java + React)', viewValue: 'Full Stack (Java + React)'},
+    {value: 'Full Stack (Python + React)', viewValue: 'Full Stack (Python + React)'},
+    {value: 'Full Stack (Python + Angular)', viewValue: 'Full Stack (Python + Angular)'},
+    {value: 'iOS', viewValue: 'iOS'},
+    {value: 'Java', viewValue: 'Java'},
+    {value: 'MEAN', viewValue: 'MEAN'},
+    {value: 'MERN', viewValue: 'MERN'},
+    {value: 'PM', viewValue: 'PM'},
+    {value: 'Python', viewValue: 'Python'},
+    {value: 'React Native', viewValue: 'React Native'},
+    {value: 'UI/UX', viewValue: 'UI/UX'},
+    {value: 'QA', viewValue: 'QA'}
+  ],
 
-  export const StaticData = {
-    ROLE_LIST: [
-        {value: 'Frontend Dev', viewValue: 'Frontend Dev'},
-        {value: 'Backend Dev', viewValue: 'Backend Dev'},
-        {value: 'DevOps', viewValue: 'DevOps'},
-        {value: 'Designer', viewValue: 'Designer'}
-      ],
+  RESOURCES_lIST: [
+    {
+      "id": 1,
+      "firstName": "Naynesh",
+      "lastName": "Rathod",
+      "email": "naynesh.rathod@mindbowser.com",
+      "team": "front end",
+      "month": "9",
+      "year": "1",
+      "addedBy": 3,
+      "technologyCtrl": ["Angular", "Node js", "Express", "Python"],
+    },
+    {
+      "id": 3,
+      "firstName": "Ravi",
+      "lastName": "patel",
+      "email": "Ravi@gmail.com",
+      "team": "front end",
+      "month": "1",
+      "year": "5",
+      "addedBy": 3,
+      "technologyCtrl": ["python", "java"],
+      "image": 'assets/images/avatars/female-02.jpg'
+    },
+    {
+      "id": 3,
+      "firstName": "Ravi",
+      "lastName": "patel",
+      "email": "Ravi@gmail.com",
+      "team": "front end",
+      "month": "1",
+      "year": "5",
+      "addedBy": 3,
+      "technologyCtrl": ["python", "java"],
+      "image": 'assets/images/avatars/female-02.jpg'
+    },
+    {
+      "id": 4,
+      "firstName": "suraj",
+      "lastName": "jaiswal",
+      "email": "suraj@gmail.com",
+      "team": "Java developer",
+      "month": "6",
+      "year": "1",
+      "addedBy": 3,
+      "technologyCtrl": ["spring boot", "java", "microservices"],
+      "image": 'assets/images/avatars/male-07.jpg'
+    },
+  ]
+};
 
-      RESOURCES_lIST:[
-        {
-          name: "Sanskriti Gupta",
-          email: "sanskriti.gupta@mindbowser.com",
-          team: "develop",
-          experience: "1 Year",
-          technology: "angular",
-          image: 'assets/images/avatars/female-02.jpg'
-        },
-        {
-          name: "Suraj",
-          email: "suraj@mindbowser.com",
-          team: "develop",
-          experience: "2 Year",
-          technology: "java",
-          image: 'assets/images/avatars/male-07.jpg'
-        },
-        {
-          name: "sans",
-          email: "sans@mindbowser.com",
-          team: "develop",
-          experience: "1 Year",
-          technology: "java",
-          image: 'assets/images/avatars/female-04.jpg'
-        }
-      ]
-  };
-  
 
-  export interface StaticInterface {
-    [key: string]: string;
-  }
-  
+export interface StaticInterface {
+  [key: string]: string;
+}
