627bee37f8e0bc818b0b7e1f7bdf980f0d4488b3
Naynesh Rathod <naynesh.rathod@mindbowser.com>
2022-07-14 19:50:20 +0530

    Get resources List API  and Css change for Mobile view

--- src/app/core/constacts/constacts.ts
+++ src/app/core/constacts/constacts.ts
@@ -1,34 +1,35 @@
-import { environment } from "../../../environments/environment";
+import {environment} from "../../../environments/environment";
+
 const baseUrl = environment.baseUrl
 const jiraBaseUrl = environment.jiraBaseUrl
 const jircloudBaseUrl = environment.cloudBaseUrl
 const projectBaseUrl = environment.projectBaseUrl
 // export class AppConstants {
 //     private static AUTH_USER_API = baseUrl;
-    
+
 // }
 export const AppConstants: ConstantsInterface = {
-    BASE_URL: baseUrl,
-    AUTH_USER_API: `${baseUrl}/signin`,
-    UPDATE_ACCESS_TOKEN: `${baseUrl}/get-access-token`,
-    CONNECT_PROJECT: `${jiraBaseUrl}/jira-project`,
-    JIRA_SYNC: `${jiraBaseUrl}/connect-jira`,
-    WORK_LOG: `${jiraBaseUrl}/work-log`,
-    GET_JIRA_USER: `${jiraBaseUrl}/jira-user`,
-    GET_TEAM_MEMBER_LIST: `${jircloudBaseUrl}/get-team-member`,
-    GET_PROJECTS: `${projectBaseUrl}/project`,
-  };
-  export const ValidationConstants = {
-    EMAIL_VALIDATION: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    NAME_VALIDATION: "^[a-zA-Z-',]+(s{0,1}[a-zA-Z-', ]){0,30}([a-zA-Z, ]){1,30}$",
-    YEAR_VALIDATION: "^[0-9]",
-  };
-  export const ErrorMessage = {
-    ERROR_FIVE_HUNDRED: "Internal Server Error"
-  };
-  
+  BASE_URL: baseUrl,
+  AUTH_USER_API: `${baseUrl}/signin`,
+  UPDATE_ACCESS_TOKEN: `${baseUrl}/get-access-token`,
+  CONNECT_PROJECT: `${jiraBaseUrl}/jira-project`,
+  JIRA_SYNC: `${jiraBaseUrl}/connect-jira`,
+  WORK_LOG: `${jiraBaseUrl}/work-log`,
+  GET_JIRA_USER: `${jiraBaseUrl}/jira-user`,
+  GET_TEAM_MEMBER_LIST: `${jircloudBaseUrl}/get-team-member`,
+  GET_RESOURCE_LIST: `${projectBaseUrl}/get-team-member`,
+  GET_PROJECTS: `${projectBaseUrl}/project`,
+};
+export const ValidationConstants = {
+  EMAIL_VALIDATION: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  NAME_VALIDATION: "^[a-zA-Z-',]+(s{0,1}[a-zA-Z-', ]){0,30}([a-zA-Z, ]){1,30}$",
+  YEAR_VALIDATION: "^[0-9]",
+};
+export const ErrorMessage = {
+  ERROR_FIVE_HUNDRED: "Internal Server Error"
+};
 
-  export interface ConstantsInterface {
-    [key: string]: string;
-  }
-  
+
+export interface ConstantsInterface {
+  [key: string]: string;
+}
--- src/app/modules/admin/resources/resources-list/resources-list.component.ts
+++ src/app/modules/admin/resources/resources-list/resources-list.component.ts
@@ -1,36 +1,43 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { StaticData } from "../../../../core/constacts/static";
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Router} from '@angular/router';
+import {StaticData} from "../../../../core/constacts/static";
 import {FormControl} from '@angular/forms';
-import { fuseAnimations } from '@fuse/animations';
+import {fuseAnimations} from '@fuse/animations';
+import {CreateProjecteService} from "@services/create-projecte.service";
+
 @Component({
   selector: 'app-resources-list',
   templateUrl: './resources-list.component.html',
   styleUrls: ['./resources-list.component.scss'],
-encapsulation  : ViewEncapsulation.None,
-changeDetection: ChangeDetectionStrategy.OnPush,
-animations     : fuseAnimations
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: fuseAnimations
 })
 export class ResourcesListComponent implements OnInit {
   technologys = new FormControl('');
-
-  technologyLIst: string[] = ['JAVA', 'Angular', 'Python', 'HTML'];
-  expriences: string[] = [
-    '0 - 1',
-    '1+',
-    '2+',
-    '3+',
-    '4+'
-  ];
+  technologyLIst: string[] = ['ALL', 'JAVA', 'Angular', 'Python', 'HTML'];
+  expriences: string[] = ['0 t0 1', '1+', '2+', '3+', '4+'];
   resources: any = StaticData.RESOURCES_lIST
   isLoading: boolean = false;
-  constructor(
-    private router: Router,
-  ) { }
+
+  constructor(private ProjectService: CreateProjecteService, private router: Router,) {
+  }
 
   ngOnInit(): void {
+    this.isLoading = true;
+    this.getList();
   }
-  gotoAddResources(){
-    this.router.navigate(['/resources/add-resources']) 
-   }
+
+  gotoAddResources() {
+    this.router.navigate(['/resources/add-resources'])
+  }
+
+  getList() {
+    this.ProjectService.getResourceMember().subscribe((res: any) => {
+      this.isLoading = false;
+    }, error => {
+      this.isLoading = false;
+    })
+    this.isLoading = false;
+  }
 }
