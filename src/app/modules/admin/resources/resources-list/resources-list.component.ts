9e25caf0577731f8a0223172d38273963cea79d8
Naynesh Rathod <naynesh.rathod@mindbowser.com>
2022-07-15 16:59:34 +0530

    Get resources List API  if data aviable or not

--- src/app/modules/admin/resources/resources-list/resources-list.component.ts
+++ src/app/modules/admin/resources/resources-list/resources-list.component.ts
@@ -1,9 +1,9 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Router} from '@angular/router';
-import {StaticData} from "../../../../core/constacts/static";
-import {FormControl} from '@angular/forms';
-import {fuseAnimations} from '@fuse/animations';
-import {CreateProjecteService} from "@services/create-projecte.service";
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+import { StaticData } from "../../../../core/constacts/static";
+import { FormControl } from '@angular/forms';
+import { fuseAnimations } from '@fuse/animations';
+import { CreateProjecteService } from "@services/create-projecte.service";
 
 @Component({
   selector: 'app-resources-list',
@@ -17,7 +17,7 @@ export class ResourcesListComponent implements OnInit {
   technologys = new FormControl('');
   technologyLIst: string[] = ['ALL', 'JAVA', 'Angular', 'Python', 'HTML'];
   expriences: string[] = ['0 t0 1', '1+', '2+', '3+', '4+'];
-  resources: any = StaticData.RESOURCES_lIST
+  resources:any=null;
   isLoading: boolean = false;
 
   constructor(private ProjectService: CreateProjecteService, private router: Router,) {
@@ -34,6 +34,8 @@ export class ResourcesListComponent implements OnInit {
 
   getList() {
     this.ProjectService.getResourceMember().subscribe((res: any) => {
+      this.resources = StaticData.RESOURCES_lIST ;
+      this.resources = this.resources.concat(res.data);
       this.isLoading = false;
     }, error => {
       this.isLoading = false;
