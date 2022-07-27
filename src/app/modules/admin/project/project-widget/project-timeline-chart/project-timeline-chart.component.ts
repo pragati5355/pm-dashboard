28990d4ce3eacc9d92379cca924c75ccd0155f36
naynesh.rathod@mindbowser.com <Nano@15894>
2022-07-27 13:43:43 +0530

    Comment  Changes Done

--- src/app/modules/admin/project/project-widget/project-timeline-chart/project-timeline-chart.component.ts
+++ src/app/modules/admin/project/project-widget/project-timeline-chart/project-timeline-chart.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { chartConfig } from 'app/core/config/chart.config';
 import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexTitleSubtitle, ApexDataLabels, ApexNonAxisChartSeries, ApexStroke, ApexLegend, ApexFill, ApexTooltip, ApexPlotOptions, ApexResponsive, ApexYAxis, ApexGrid, ApexStates, ApexTheme, ApexAnnotations } from "ng-apexcharts";
 
 @Component({
@@ -9,9 +10,9 @@ import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexTitleSubtitle, ApexDataL
 
 export class ProjectTimelineChartComponent implements OnInit {
   @Input() fill: ApexFill;
-  @Input() chart: ApexChart;
-  @Input() xaxis: ApexXAxis;
-  @Input() legend: ApexLegend;
+  @Input() chart: ApexChart | any;
+  @Input() xaxis: ApexXAxis | any;
+  @Input() legend: ApexLegend | any;
   @Input() plotOptions: ApexPlotOptions;
   @Input() series: ApexAxisChartSeries | ApexNonAxisChartSeries;
   // @Input() labels: string[];
@@ -29,151 +30,12 @@ export class ProjectTimelineChartComponent implements OnInit {
   // @Input() theme: ApexTheme;
 
   constructor() {
-    this.series = [
-      {
-        name: "Bob",
-        data: [
-          {
-            x: "Design",
-            y: [
-              new Date("2019-03-05").getTime(),
-              new Date("2019-03-08").getTime()
-            ]
-          },
-          {
-            x: "Code",
-            y: [
-              new Date("2019-03-02").getTime(),
-              new Date("2019-03-05").getTime()
-            ]
-          },
-          {
-            x: "Code",
-            y: [
-              new Date("2019-03-05").getTime(),
-              new Date("2019-03-07").getTime()
-            ]
-          },
-          {
-            x: "Test",
-            y: [
-              new Date("2019-03-03").getTime(),
-              new Date("2019-03-09").getTime()
-            ]
-          },
-          {
-            x: "Test",
-            y: [
-              new Date("2019-03-08").getTime(),
-              new Date("2019-03-11").getTime()
-            ]
-          },
-          {
-            x: "Validation",
-            y: [
-              new Date("2019-03-11").getTime(),
-              new Date("2019-03-16").getTime()
-            ]
-          },
-          {
-            x: "Design",
-            y: [
-              new Date("2019-03-01").getTime(),
-              new Date("2019-03-03").getTime()
-            ]
-          }
-        ]
-      },
-      {
-        name: "Joe",
-        data: [
-          {
-            x: "Design",
-            y: [
-              new Date("2019-03-02").getTime(),
-              new Date("2019-03-05").getTime()
-            ]
-          },
-          {
-            x: "Test",
-            y: [
-              new Date("2019-03-06").getTime(),
-              new Date("2019-03-16").getTime()
-            ]
-          },
-          {
-            x: "Code",
-            y: [
-              new Date("2019-03-03").getTime(),
-              new Date("2019-03-07").getTime()
-            ]
-          },
-          {
-            x: "Deployment",
-            y: [
-              new Date("2019-03-20").getTime(),
-              new Date("2019-03-22").getTime()
-            ]
-          },
-          {
-            x: "Design",
-            y: [
-              new Date("2019-03-10").getTime(),
-              new Date("2019-03-16").getTime()
-            ]
-          }
-        ]
-      },
-      {
-        name: "Dan",
-        data: [
-          {
-            x: "Code",
-            y: [
-              new Date("2019-03-10").getTime(),
-              new Date("2019-03-17").getTime()
-            ]
-          },
-          {
-            x: "Validation",
-            y: [
-              new Date("2019-03-05").getTime(),
-              new Date("2019-03-09").getTime()
-            ]
-          }
-        ]
-      }
-    ];
-    this.chart = {
-      height: 350,
-      type: "rangeBar"
-    },
-      this.plotOptions = {
-        bar: {
-          horizontal: true,
-          barHeight: "80%"
-        }
-      };
-    this.xaxis = {
-      type: "datetime"
-    };
-    this.fill = {
-      type: "gradient",
-      gradient: {
-        shade: "light",
-        type: "vertical",
-        shadeIntensity: 0.25,
-        gradientToColors: undefined,
-        inverseColors: true,
-        opacityFrom: 1,
-        opacityTo: 1,
-        stops: [50, 0, 100, 100]
-      }
-    };
-    this.legend = {
-      position: "top",
-      horizontalAlign: "left"
-    };
+    this.series = chartConfig.PIE_CHART[0].series;
+    this.chart = chartConfig.PIE_CHART[0].chart;
+    this.plotOptions = chartConfig.PIE_CHART[0].plotOptions;
+    this.xaxis = chartConfig.PIE_CHART[0].xaxis
+    this.fill = chartConfig.PIE_CHART[0].fill;
+    this.legend = chartConfig.PIE_CHART[0].legend;
   }
 
   ngOnInit() {
