fd9a5c4b462b7bb219d0f9c77e917a753d44bd36
naynesh.rathod@mindbowser.com <Nano@15894>
2022-08-04 13:05:29 +0530

    Change Some POint For Project Details

--- src/app/modules/admin/project/project-widget/project-details/project-details.component.ts
+++ src/app/modules/admin/project/project-widget/project-details/project-details.component.ts
@@ -1,63 +1,61 @@
-import { members } from './../../../../../mock-api/apps/scrumboard/data';
-import { Component, ElementRef, OnInit, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
-import { FuseCardComponent } from '@fuse/components/card';
-import { ActivatedRoute, Router } from '@angular/router';
+import {Component, ElementRef, Input, OnInit, QueryList, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {SnackBar} from '../../../../../core/utils/snackBar'
-import {  Input } from '@angular/core'
-import { CreateProjecteService } from '@services/create-projecte.service';
+import {CreateProjecteService} from '@services/create-projecte.service';
+
 @Component({
-  selector: 'app-project-details',
-  templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-project-details',
+    templateUrl: './project-details.component.html',
+    styleUrls: ['./project-details.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class ProjectDetailsComponent implements OnInit {
-  project_name = "This is a Project name";
-  project_status = "On Track";
-  project_progres = 45;
-  members=true;
-  sprints=true;
-  routeSubscribe: any;
-  projectId = 0
-  initialLoading= false
-  @Input() dataId: any ;
-  constructor(private router: Router,
-    private _route: ActivatedRoute,
-    private ProjectService:CreateProjecteService,
-    private snackBar: SnackBar) { }
-  private _fuseCards!: QueryList<ElementRef>;
-  ngOnInit(): void {
-    console.log('app-project-details')
-    this.routeSubscribe = this._route.queryParams.subscribe(projectId => {
-      if (projectId['id']) {
-          this.projectId = projectId['id']
-      }
-    });
-    this.getProjectDetails()
-  }
-  editProject() {
-    this.router.navigate(
-      [`/projects/edit-project`],
-      {queryParams: {id: this.projectId}}
-    );
-  }
-  getProjectDetails(){
-    // let res = StaticData.PROJECT_DETAILS
-    let payload ={
-      id: this.projectId 
+    project_name = "This is a Project name";
+    project_status = "On Track";
+    project_progres = 45;
+    members = true;
+    sprints = true;
+    routeSubscribe: any;
+    projectId = 0
+    initialLoading = false
+    @Input() dataId: any;
+    private _fuseCards!: QueryList<ElementRef>;
+
+    constructor(private router: Router, private _route: ActivatedRoute, private ProjectService: CreateProjecteService, private snackBar: SnackBar) {
     }
-    this.initialLoading = true;
-    this.ProjectService.getOneProjectDetails(payload).subscribe(
-      (res: any) => {
-        this.initialLoading = false;
-          console.log(res)    
-          this.project_name = res.data.project.name
- 
-        
- 
-      }, (error: any) => {
-        this.initialLoading = false;
-      });
-  
-  }
+
+    ngOnInit(): void {
+        console.log('app-project-details')
+        this.routeSubscribe = this._route.queryParams.subscribe(projectId => {
+            if (projectId['id']) {
+                this.projectId = projectId['id']
+            }
+        });
+        this.getProjectDetails()
+    }
+
+    editProject() {
+        this.router.navigate(
+            [`/projects/edit-project`],
+            {queryParams: {id: this.projectId}}
+        );
+    }
+
+    getProjectDetails() {
+        // let res = StaticData.PROJECT_DETAILS
+        let payload = {
+            id: this.projectId
+        }
+        this.initialLoading = true;
+        this.ProjectService.getOneProjectDetails(payload).subscribe(
+            (res: any) => {
+                this.initialLoading = false;
+                console.log(res)
+                this.project_name = res.data.project.name
+
+            }, (error: any) => {
+                this.initialLoading = false;
+            });
+
+    }
 }
