fe4bbab182065c968fff817357f6f1e299a95b5b
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2022-09-06 11:35:16 +0530

    add project changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   jiraBaseUrl: 'https://3znje7eqrk.execute-api.ap-south-1.amazonaws.com',
   projectBaseUrl: 'https://cu1azpqty2.execute-api.ap-south-1.amazonaws.com',
   jirasyncBaseUrl:' https://us773ifzr2h6ih5h4imvu5sspy0ixbhc.lambda-url.ap-south-1.on.aws',
-  GoogleClientId: "634085334801-2qo990of3dib17r84cdqa1rn2h5ur0ju.apps.googleusercontent.com"
-  // GoogleClientId: "348103697312-ibthsgsun4vi3mcpa6i078f513ed6sq3.apps.googleusercontent.com"
+  // GoogleClientId: "634085334801-2qo990of3dib17r84cdqa1rn2h5ur0ju.apps.googleusercontent.com"
+  GoogleClientId: "348103697312-ibthsgsun4vi3mcpa6i078f513ed6sq3.apps.googleusercontent.com"
 };
