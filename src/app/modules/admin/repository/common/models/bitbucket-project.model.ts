637625ef97da787a175402d98594d6620107e0da
Rahul Dudhane <rahul.dudhane@mindbowser.com>
2023-01-24 15:03:35 +0530

    model updated

--- src/app/modules/admin/repository/common/models/bitbucket-project.model.ts
+++ src/app/modules/admin/repository/common/models/bitbucket-project.model.ts
@@ -1,12 +1,5 @@
 export class BitbucketProjectModel {
-    id: number;
     uuid: any;
-    projectName: string;
-    bitbucketProjectName: string;
+    name: string;
     key: string;
-    createdAt: string;
-    createdBy: string;
-    bitbucketUrl: string;
-    jenkinsUrl: any;
-    status: string;
 }
