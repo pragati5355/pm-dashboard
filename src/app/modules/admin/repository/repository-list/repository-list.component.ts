c9a13582fe60946eea6fc47adb159005229191de
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-02-10 14:50:42 +0530

    add function in repository list

--- src/app/modules/admin/repository/repository-list/repository-list.component.ts
+++ src/app/modules/admin/repository/repository-list/repository-list.component.ts
@@ -1,44 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
-  selector: 'app-repository-list',
-  templateUrl: './repository-list.component.html',
-  styleUrls: ['./repository-list.component.scss']
+    selector: 'app-repository-list',
+    templateUrl: './repository-list.component.html',
+    styleUrls: ['./repository-list.component.scss'],
 })
 export class RepositoryListComponent implements OnInit {
-  initialLoading = false
-  totalRecored = 1
-  repository: any = []
-  constructor() { }
+    initialLoading = false;
+    totalRecored = 1;
+    repository: any = [];
+    constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.repository = [
-      {
-        bitbucketProjectName: "demo",
-        projectName:"indbowser",
-        key: "GINGER14",
-        bitbucketUrl:"GINGER14",
-        createdAt: "2021-11-18",
-        createdBy: "sanskriti",
-        jenkinsUrl: "kj",
-        status:"success",
-        console:"console"
-
-      },
-      {
-        bitbucketProjectName: "demo",
-        projectName:"indbowser",
-        key: "GINGER14",
-        bitbucketUrl:"GINGER14",
-        createdAt: "2021-11-18",
-        createdBy: "sanskriti",
-        jenkinsUrl: "kj",
-        status:"success",
-        console:"console"
-
-      },
-      
-    ]
-  }
-
+    ngOnInit(): void {
+        this.repository = [
+            {
+                bitbucketProjectName: 'demo',
+                projectName: 'mindbowser',
+                key: 'GINGER14',
+                bitbucketUrl: 'GINGER14',
+                createdAt: '2021-11-18',
+                createdBy: 'sanskriti',
+                jenkinsUrl: 'kj',
+                status: 'success',
+                console: 'console',
+            },
+            {
+                bitbucketProjectName: 'demo',
+                projectName: 'mindbowser',
+                key: 'GINGER14',
+                bitbucketUrl: 'GINGER14',
+                createdAt: '2021-11-18',
+                createdBy: 'sanskriti',
+                jenkinsUrl: 'kj',
+                status: 'success',
+                console: 'console',
+            },
+        ];
+    }
+    createRepository() {
+        this.router.navigate(['/projects/repository/repository-list']);
+    }
 }
