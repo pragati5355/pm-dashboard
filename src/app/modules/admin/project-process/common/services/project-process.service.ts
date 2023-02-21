cc84c3f33ceb4c840492e261404227cd2c118379
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-02-21 10:55:47 +0530

    add project process list

--- src/app/modules/admin/project-process/common/services/project-process.service.ts
+++ src/app/modules/admin/project-process/common/services/project-process.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from '../../../../../core/constacts/constacts';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ProjectProcessService {
-
-  constructor() { }
+    constructor(private http: HttpClient) {}
+    getProcessForm = AppConstants['PROJECT_API_URL'] + '/checklist-form';
+    saveProcessForm = AppConstants['PROJECT_API_URL'] + '/checklist';
+    submittedProcessForm =
+        AppConstants['PROJECT_API_URL'] + '/checklist-response';
+    find() {
+        return this.http.get(this.getProcessForm);
+    }
+    create(obj: any) {
+        return this.http.post(this.saveProcessForm, obj);
+    }
+    submittedForm(obj: any) {
+        return this.http.post(this.submittedProcessForm, obj);
+    }
 }
--- src/app/modules/admin/project-process/project-process-list/project-process-list.component.ts
+++ src/app/modules/admin/project-process/project-process-list/project-process-list.component.ts
@@ -1,12 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ProcessFormComponent } from '../process-form/process-form.component';
 @Component({
     selector: 'app-project-process-list',
     templateUrl: './project-process-list.component.html',
     styleUrls: ['./project-process-list.component.scss'],
 })
 export class ProjectProcessListComponent implements OnInit {
-    constructor() {}
+    initialLoading = false;
+    totalRecored = 1;
+    repository: any = [];
+    constructor(private dialog: MatDialog) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.repository = [
+            {
+                bitbucketProjectName: 'demo',
+                projectName: 'indbowser',
+                key: 'GINGER14',
+                bitbucketUrl: 'GINGER14',
+                createdAt: '2021-11-18',
+                createdBy: 'sanskriti',
+                jenkinsUrl: 'kj',
+                status: 'success',
+                console: 'console',
+            },
+            {
+                bitbucketProjectName: 'demo',
+                projectName: 'indbowser',
+                key: 'GINGER14',
+                bitbucketUrl: 'GINGER14',
+                createdAt: '2021-11-18',
+                createdBy: 'sanskriti',
+                jenkinsUrl: 'kj',
+                status: 'success',
+                console: 'console',
+            },
+        ];
+    }
+    processForm() {
+        const dialogRef = this.dialog.open(ProcessFormComponent, {
+            disableClose: true,
+            panelClass: 'warn-dialog-content',
+            autoFocus: false,
+        });
+        dialogRef.afterClosed().subscribe((result: any) => {
+            if (result.result == 'success') {
+            }
+        });
+    }
+    processFormSubmitted() {
+        const dialogRef = this.dialog.open(ProcessFormComponent, {
+            disableClose: true,
+            panelClass: 'warn-dialog-content',
+            autoFocus: false,
+            data: 'submitted',
+        });
+        dialogRef.afterClosed().subscribe((result: any) => {
+            if (result.result == 'success') {
+            }
+        });
+    }
 }
