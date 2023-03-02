40f101142b95a06c3e065834ac3b19028dc6687a
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-03-02 19:10:03 +0530

    api changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     jiraBaseUrl: 'https://m2xrqfnhd1.execute-api.ap-south-1.amazonaws.com',
     projectBaseUrl: 'https://s1pp7u6klg.execute-api.ap-south-1.amazonaws.com',
     springBootUrl:
-        'http://ec2-3-110-78-220.ap-south-1.compute.amazonaws.com:8001/api/v1',
+        'https://metrics-758789250.ap-south-1.elb.amazonaws.com//api/v1',
     jirasyncBaseUrl:
         'https://xng2jrnm6tmjcc3zh6dtvbqu4i0zdwja.lambda-url.ap-south-1.on.aws',
     GoogleClientId:
