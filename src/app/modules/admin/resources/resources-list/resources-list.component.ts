f804bebc2322c579b7f77ac6caade22c8c3c2c91
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-03-08 19:40:47 +0530

    Code cleanup

--- src/app/modules/admin/resources/resources-list/resources-list.component.ts
+++ src/app/modules/admin/resources/resources-list/resources-list.component.ts
@@ -92,132 +92,9 @@ export class ResourcesListComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.exprienceForm = this._formBuilder.group(
-            {
-                minExprience: [
-                    '',
-                    [Validators.pattern(ValidationConstants.YEAR_VALIDATION)],
-                ],
-                maxExprience: [
-                    '',
-                    [Validators.pattern(ValidationConstants.YEAR_VALIDATION)],
-                ],
-            },
-            {
-                validator: [
-                    ExprienceValidation('minExprience', 'maxExprience'),
-                ],
-            }
-        );
-        const expriencePayload = [
-            parseInt(this.exprienceForm?.value?.minExprience),
-            parseInt(this.exprienceForm?.value?.maxExprience),
-        ];
-        const payload = {
-            technology:
-                this.technologys?.value.length > 0
-                    ? this.technologys?.value
-                    : null,
-            experience:
-                this.exprienceForm?.value?.minExprience.length > 0 &&
-                this.exprienceForm?.value?.maxExprience.length > 0
-                    ? expriencePayload
-                    : null,
-            projects: this.projects?.value ? [this.projects.value] : null,
-            perPageData: this.count,
-            totalPerPageData: this.totalPerPageData,
-            name: this.searchValue,
-        };
-        this.getList(payload);
-        this.getListtechList();
-        this.getProjectList();
-        // Build the config form
-        this.configForm = this._formBuilder.group({
-            title: 'Delete Resource',
-            message:
-                'Are you sure you want to delete this resource? <span class="font-medium">This action cannot be undone!</span>',
-            icon: this._formBuilder.group({
-                show: true,
-                name: 'heroicons_outline:exclamation',
-                color: 'warn',
-            }),
-            actions: this._formBuilder.group({
-                confirm: this._formBuilder.group({
-                    show: true,
-                    label: 'Delete',
-                    color: 'warn',
-                }),
-                cancel: this._formBuilder.group({
-                    show: true,
-                    label: 'Cancel',
-                }),
-            }),
-            dismissible: false,
-        });
-        this.matDrawer.openedChange.subscribe((opened) => {
-            if (!opened) {
-                // Remove the selected contact when drawer closed
-                this.selectedContact = null;
-
-                // Mark for check
-                this._changeDetectorRef.markForCheck();
-            }
-        });
-        this._fuseMediaWatcherService.onMediaChange$
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(({ matchingAliases }) => {
-                // Set the drawerMode if the given breakpoint is active
-                if (matchingAliases.includes('lg')) {
-                    this.drawerMode = 'side';
-                } else {
-                    this.drawerMode = 'over';
-                }
-
-                // Mark for check
-                this._changeDetectorRef.markForCheck();
-            });
-
-        /** Below code handles resource search input with debounce time of 1000ms */
-        this.resourceSearchInput.valueChanges
-            .pipe(
-                debounceTime(1000),
-                distinctUntilChanged(),
-                switchMap((inputChanged) => {
-                    this.count = 1;
-                    this.pagination = false;
-                    const expriencePayload = [
-                        parseInt(this.exprienceForm.value.minExprience),
-                        parseInt(this.exprienceForm.value.maxExprience),
-                    ];
-                    const payload = {
-                        technology:
-                            this.technologys.value.length > 0
-                                ? this.technologys.value
-                                : null,
-                        experience:
-                            this.exprienceForm.value.minExprience.length > 0 &&
-                            this.exprienceForm.value.maxExprience.length > 0
-                                ? expriencePayload
-                                : null,
-                        projects: this.projects?.value
-                            ? [this.projects.value]
-                            : null,
-                        perPageData: this.count,
-                        totalPerPageData: this.totalPerPageData,
-                        name: inputChanged.trim(),
-                    };
-                    return this.ProjectService.getResourceMember(payload);
-                })
-            )
-            .subscribe(
-                (res: any) => {
-                    this.handleGetResourceMemberResponse(res);
-                },
-                (error) => {
-                    this.totalRecored = 0;
-                    this.initialLoading = false;
-                }
-            );
+        this.loadData();
+        this.initializeForms();
+        this.addRequiredSubscriptions();
     }
 
     get exprienceValidForm(): { [key: string]: AbstractControl } {
@@ -228,9 +105,12 @@ export class ResourcesListComponent implements OnInit {
         this.router.navigate(['/resources/add-resources']);
     }
 
-    getList(payload: any) {
+    getList(searchPayload?: any) {
+        if (!searchPayload) {
+            searchPayload = this.getDefaultSearchPayload();
+        }
         this.initialLoading = true;
-        this.ProjectService.getResourceMember(payload).subscribe(
+        this.ProjectService.getResourceMember(searchPayload).subscribe(
             (res: any) => {
                 this.handleGetResourceMemberResponse(res);
             },
@@ -241,7 +121,7 @@ export class ResourcesListComponent implements OnInit {
         );
     }
 
-    getListtechList() {
+    getTechnologies() {
         this.ProjectService.getTechnology().subscribe(
             (res: any) => {
                 this.technologyLIst = res?.data;
@@ -270,21 +150,7 @@ export class ResourcesListComponent implements OnInit {
                 parseInt(this.exprienceForm?.value?.minExprience),
                 parseInt(this.exprienceForm?.value?.maxExprience),
             ];
-            const payload = {
-                perPageData: this.count,
-                totalPerPageData: this.totalPerPageData,
-                technology:
-                    this.technologys?.value.length > 0
-                        ? this.technologys.value
-                        : null,
-                experience:
-                    this.exprienceForm?.value?.minExprience.length > 0 &&
-                    this.exprienceForm?.value?.maxExprience.length > 0
-                        ? expriencePayload
-                        : null,
-                projects: this.projects?.value ? [this.projects.value] : null,
-                name: this.searchValue,
-            };
+            const payload = this.getDefaultSearchPayload(this.count);
             this.pagination = true;
             this.initialLoading = true;
             this.ProjectService.getResourceMember(payload).subscribe(
@@ -345,36 +211,6 @@ export class ResourcesListComponent implements OnInit {
         );
     }
 
-    inputSearch(event: any) {
-        console.log('Search value :', this.searchValue);
-        console.log('value :', event.target.value.trim());
-        this.count = 1;
-        this.pagination = false;
-        const expriencePayload = [
-            parseInt(this.exprienceForm.value.minExprience),
-            parseInt(this.exprienceForm.value.maxExprience),
-        ];
-        if (this.searchValue !== event.target.value.trim()) {
-            this.searchValue = event.target.value.trim();
-            const payload = {
-                technology:
-                    this.technologys.value.length > 0
-                        ? this.technologys.value
-                        : null,
-                experience:
-                    this.exprienceForm.value.minExprience.length > 0 &&
-                    this.exprienceForm.value.maxExprience.length > 0
-                        ? expriencePayload
-                        : null,
-                projects: this.projects?.value ? [this.projects.value] : null,
-                perPageData: this.count,
-                totalPerPageData: this.totalPerPageData,
-                name: this.searchValue,
-            };
-            this.getList(payload);
-        }
-    }
-
     deleteResource(id: number, assignedProjects: any): void {
         this.deleteprojectstring(assignedProjects);
         if (assignedProjects.length == 0) {
@@ -417,35 +253,8 @@ export class ResourcesListComponent implements OnInit {
                                     this.snackBar.successSnackBar(
                                         res.data.Message
                                     );
-                                    const expriencePayload = [
-                                        parseInt(
-                                            this.exprienceForm.value
-                                                .minExprience
-                                        ),
-                                        parseInt(
-                                            this.exprienceForm.value
-                                                .maxExprience
-                                        ),
-                                    ];
-                                    const payload = {
-                                        technology:
-                                            this.technologys.value.length > 0
-                                                ? this.technologys.value
-                                                : null,
-                                        experience:
-                                            this.exprienceForm.value
-                                                .minExprience.length > 0 &&
-                                            this.exprienceForm.value
-                                                .maxExprience.length > 0
-                                                ? expriencePayload
-                                                : null,
-                                        projects: this.projects.value
-                                            ? [this.projects.value]
-                                            : null,
-                                        perPageData: this.count,
-                                        totalPerPageData: this.totalPerPageData,
-                                        name: this.searchValue,
-                                    };
+                                    const payload =
+                                        this.getDefaultSearchPayload();
                                     this.getList(payload);
                                 },
                                 (error) => {
@@ -510,27 +319,9 @@ export class ResourcesListComponent implements OnInit {
             });
         }
         if (!this.exprienceForm.invalid) {
-            const expriencePayload = [
-                parseInt(this.exprienceForm.value.minExprience),
-                parseInt(this.exprienceForm.value.maxExprience),
-            ];
             this.minExprience = this.exprienceForm.value.minExprience;
             this.maxExprience = this.exprienceForm.value.maxExprience;
-            let payload = {
-                technology:
-                    this.technologys.value.length > 0
-                        ? this.technologys.value
-                        : null,
-                experience:
-                    this.exprienceForm.value.minExprience.length > 0 &&
-                    this.exprienceForm.value.maxExprience.length > 0
-                        ? expriencePayload
-                        : null,
-                projects: this.projects.value ? [this.projects.value] : null,
-                perPageData: this.count,
-                totalPerPageData: this.totalPerPageData,
-                name: this.searchValue,
-            };
+            let payload = this.getDefaultSearchPayload(this.count);
             this.ProjectService.getResourceMember(payload).subscribe(
                 (res: any) => {
                     this.handleGetResourceMemberResponse(res);
@@ -560,25 +351,7 @@ export class ResourcesListComponent implements OnInit {
     selectChangeProject(event: any) {
         this.count = 1;
         this.pagination = false;
-        let expriencePayload = [
-            parseInt(this.exprienceForm.value.minExprience),
-            parseInt(this.exprienceForm.value.maxExprience),
-        ];
-        let payload = {
-            technology:
-                this.technologys.value.length > 0
-                    ? this.technologys.value
-                    : null,
-            experience:
-                this.exprienceForm.value.minExprience.length > 0 &&
-                this.exprienceForm.value.maxExprience.length > 0
-                    ? expriencePayload
-                    : null,
-            projects: this.projects.value ? [this.projects.value] : null,
-            perPageData: this.count,
-            totalPerPageData: this.totalPerPageData,
-            name: this.searchValue,
-        };
+        let payload = this.getDefaultSearchPayload(this.count);
         this.ProjectService.getResourceMember(payload).subscribe(
             (res: any) => {
                 this.handleGetResourceMemberResponse(res);
@@ -607,5 +380,164 @@ export class ResourcesListComponent implements OnInit {
 
     private handleTokenExpiry() {
         this._authService.updateAndReload(window.location);
+    }
+
+    private getExperiencePayload() {
+        return [
+            parseInt(this.exprienceForm?.value?.minExprience),
+            parseInt(this.exprienceForm?.value?.maxExprience),
+        ];
+    }
+
+    private getDefaultSearchPayload(count?: any) {
+        const expriencePayload = this.getExperiencePayload();
+        return {
+            technology:
+                this.technologys?.value.length > 0
+                    ? this.technologys?.value
+                    : null,
+            experience:
+                this.exprienceForm?.value?.minExprience.length > 0 &&
+                this.exprienceForm?.value?.maxExprience.length > 0
+                    ? expriencePayload
+                    : null,
+            projects: this.projects?.value ? [this.projects.value] : null,
+            perPageData: this.count,
+            totalPerPageData: this.totalPerPageData,
+            name: this.searchValue,
+        };
+    }
+
+    private initializeExperienceForm() {
+        this.exprienceForm = this._formBuilder.group(
+            {
+                minExprience: [
+                    '',
+                    [Validators.pattern(ValidationConstants.YEAR_VALIDATION)],
+                ],
+                maxExprience: [
+                    '',
+                    [Validators.pattern(ValidationConstants.YEAR_VALIDATION)],
+                ],
+            },
+            {
+                validator: [
+                    ExprienceValidation('minExprience', 'maxExprience'),
+                ],
+            }
+        );
+    }
+
+    private addRequiredSubscriptions() {
+        this.addDrawerOpenChangeSubscription();
+        this.addWatcherSubscription();
+        this.addSearchValueChangeSubscription();
+    }
+
+    private initializeForms() {
+        this.initializeExperienceForm();
+        this.initializeConfigForm();
+    }
+
+    private loadData() {
+        this.getList();
+        this.getTechnologies();
+        this.getProjectList();
+    }
+
+    private addSearchValueChangeSubscription() {
+        this.resourceSearchInput.valueChanges
+            .pipe(
+                debounceTime(1000),
+                distinctUntilChanged(),
+                switchMap((inputChanged) => {
+                    this.count = 1;
+                    this.pagination = false;
+                    const expriencePayload = [
+                        parseInt(this.exprienceForm.value.minExprience),
+                        parseInt(this.exprienceForm.value.maxExprience),
+                    ];
+                    const payload = {
+                        technology:
+                            this.technologys.value.length > 0
+                                ? this.technologys.value
+                                : null,
+                        experience:
+                            this.exprienceForm.value.minExprience.length > 0 &&
+                            this.exprienceForm.value.maxExprience.length > 0
+                                ? expriencePayload
+                                : null,
+                        projects: this.projects?.value
+                            ? [this.projects.value]
+                            : null,
+                        perPageData: this.count,
+                        totalPerPageData: this.totalPerPageData,
+                        name: inputChanged.trim(),
+                    };
+                    return this.ProjectService.getResourceMember(payload);
+                })
+            )
+            .subscribe(
+                (res: any) => {
+                    this.handleGetResourceMemberResponse(res);
+                },
+                (error) => {
+                    this.totalRecored = 0;
+                    this.initialLoading = false;
+                }
+            );
+    }
+
+    private addWatcherSubscription() {
+        this._fuseMediaWatcherService.onMediaChange$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(({ matchingAliases }) => {
+                // Set the drawerMode if the given breakpoint is active
+                if (matchingAliases.includes('lg')) {
+                    this.drawerMode = 'side';
+                } else {
+                    this.drawerMode = 'over';
+                }
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            });
+    }
+
+    private addDrawerOpenChangeSubscription() {
+        this.matDrawer.openedChange.subscribe((opened) => {
+            if (!opened) {
+                // Remove the selected contact when drawer closed
+                this.selectedContact = null;
+
+                // Mark for check
+                this._changeDetectorRef.markForCheck();
+            }
+        });
+    }
+
+    private initializeConfigForm() {
+        this.configForm = this._formBuilder.group({
+            title: 'Delete Resource',
+            message:
+                'Are you sure you want to delete this resource? <span class="font-medium">This action cannot be undone!</span>',
+            icon: this._formBuilder.group({
+                show: true,
+                name: 'heroicons_outline:exclamation',
+                color: 'warn',
+            }),
+            actions: this._formBuilder.group({
+                confirm: this._formBuilder.group({
+                    show: true,
+                    label: 'Delete',
+                    color: 'warn',
+                }),
+                cancel: this._formBuilder.group({
+                    show: true,
+                    label: 'Cancel',
+                }),
+            }),
+            dismissible: false,
+        });
     }
 }
