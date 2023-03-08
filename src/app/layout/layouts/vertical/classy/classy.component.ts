d3447186097b9d2410c9e38d4b44870001572f7b
Rahul Dudhane <rahul.dudhane@mindbowser.com>
2023-03-08 21:01:38 +0530

    Added page title in toolbar

--- src/app/layout/layouts/vertical/classy/classy.component.ts
+++ src/app/layout/layouts/vertical/classy/classy.component.ts
@@ -1,41 +1,43 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
-import { AuthService } from "@services/auth/auth.service";
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, mergeMap, Subject, takeUntil } from 'rxjs';
+import { AuthService } from '@services/auth/auth.service';
 import { FuseMediaWatcherService } from '@fuse/services/media-watcher';
-import { FuseNavigationService, FuseVerticalNavigationComponent } from '@fuse/components/navigation';
+import {
+    FuseNavigationService,
+    FuseVerticalNavigationComponent,
+} from '@fuse/components/navigation';
 import { Navigation } from 'app/core/navigation/navigation.types';
 import { NavigationService } from 'app/core/navigation/navigation.service';
 import { User } from 'app/core/user/user.types';
 import { UserService } from 'app/core/user/user.service';
 
 @Component({
-    selector     : 'classy-layout',
-    templateUrl  : './classy.component.html',
-    encapsulation: ViewEncapsulation.None
+    selector: 'classy-layout',
+    templateUrl: './classy.component.html',
+    encapsulation: ViewEncapsulation.None,
 })
-export class ClassyLayoutComponent implements OnInit, OnDestroy
-{
+export class ClassyLayoutComponent implements OnInit, OnDestroy {
     isScreenSmall: boolean | undefined;
     navigation!: Navigation;
     user!: User;
-    private _unsubscribeAll: Subject<any> = new Subject<any>();
     userData: any;
-    userPhoto: any = "";
+    pageTitle: string;
+    userPhoto: any = '';
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
     /**
      * Constructor
      */
     constructor(
-        private _activatedRoute: ActivatedRoute,
-        private _router: Router,
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
         private _navigationService: NavigationService,
         private _userService: UserService,
         private _fuseMediaWatcherService: FuseMediaWatcherService,
         private _fuseNavigationService: FuseNavigationService,
-        private authService: AuthService,
-        private router: Router
-    )
-    {
+        private authService: AuthService
+    ) {
+        this.addRouteChangeSubscription(activatedRoute);
     }
 
     // -----------------------------------------------------------------------------------------------------
@@ -45,8 +47,7 @@ export class ClassyLayoutComponent implements OnInit, OnDestroy
     /**
      * Getter for current year
      */
-    get currentYear(): number
-    {
+    get currentYear(): number {
         return new Date().getFullYear();
     }
 
@@ -57,40 +58,19 @@ export class ClassyLayoutComponent implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
-        // Subscribe to navigation data
-        this._navigationService.navigation$
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe((navigation: Navigation) => {
-                this.navigation = navigation;
-            });
-
-        // Subscribe to the user service
-        this._userService.user$
-            .pipe((takeUntil(this._unsubscribeAll)))
-            .subscribe((user: User) => {
-                this.user = user;
-            });
-
-        // Subscribe to media changes
-        this._fuseMediaWatcherService.onMediaChange$
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(({matchingAliases}) => {
-
-                // Check if the screen is small
-                this.isScreenSmall = !matchingAliases.includes('md');
-            });
-            this.userData = this.authService.getUser();
-            this.userPhoto= this.authService.getUserPhoto(); 
+    ngOnInit(): void {
+        this.extractRouteData(this.activatedRoute);
+        this.addNavigationSubscription();
+        this.addUserSubscription();
+        this.addWatcherSubscription();
+        this.userData = this.authService.getUser();
+        this.userPhoto = this.authService.getUserPhoto();
     }
 
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
+    ngOnDestroy(): void {
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
     }
@@ -104,15 +84,54 @@ export class ClassyLayoutComponent implements OnInit, OnDestroy
      *
      * @param name
      */
-    toggleNavigation(name: string): void
-    {
-        // Get the navigation
-        const navigation = this._fuseNavigationService.getComponent<FuseVerticalNavigationComponent>(name);
+    toggleNavigation(name: string): void {
+        const navigation =
+            this._fuseNavigationService.getComponent<FuseVerticalNavigationComponent>(
+                name
+            );
 
-        if ( navigation )
-        {
-            // Toggle the opened status
+        if (navigation) {
             navigation.toggle();
         }
+    }
+
+    private addWatcherSubscription() {
+        this._fuseMediaWatcherService.onMediaChange$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(({ matchingAliases }) => {
+                // Check if the screen is small
+                this.isScreenSmall = !matchingAliases.includes('md');
+            });
+    }
+
+    private addUserSubscription() {
+        this._userService.user$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((user: User) => {
+                this.user = user;
+            });
+    }
+
+    private addNavigationSubscription() {
+        this._navigationService.navigation$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((navigation: Navigation) => {
+                this.navigation = navigation;
+            });
+    }
+
+    private addRouteChangeSubscription(activatedRoute: ActivatedRoute) {
+        this.router.events
+            .pipe(filter((e) => e instanceof NavigationEnd))
+            .forEach((e) => {
+                this.extractRouteData(activatedRoute);
+            });
+    }
+
+    private extractRouteData(activatedRoute: ActivatedRoute) {
+        while (activatedRoute.firstChild) {
+            activatedRoute = activatedRoute.firstChild;
+        }
+        this.pageTitle = activatedRoute?.snapshot?.data['pageTitle'];
     }
 }
