b248459d379c5dabebc4b825b692c1c98619a6cb
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-03-10 13:36:11 +0530

    Updated dashboard UI

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@services/auth/auth.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
+    userName: string;
+    userImageUrl: string;
 
-  constructor() { }
+    constructor(private _authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.loadUserData();
+    }
+    loadUserData() {
+        const user = this._authService.getUser();
+        this.userName = `${user.firstName ? user.firstName : ' '} ${
+            user.lastName ? user.lastName : ' '
+        }`;
+        this.userImageUrl = this._authService.getUserPhoto();
+    }
+    goToProjects() {
+        this.router.navigate(['/projects/project-list']);
+    }
+    goToResources() {
+        this.router.navigate(['/resources/resources-list']);
+    }
 }
