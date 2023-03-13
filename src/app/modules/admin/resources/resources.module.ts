a0e56da4cf727910dbb670a2027b1756ff1df038
Rahul Dudhane <rahul.dudhane@mindbowser.com>
2023-03-13 19:55:56 +0530

    Page title added in resources

--- src/app/modules/admin/resources/resources.module.ts
+++ src/app/modules/admin/resources/resources.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { DatePipe } from "@angular/common";
+import { DatePipe } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -29,72 +29,78 @@ import { SharedModule } from 'app/core/shared.module';
 import { ResourcesHomeComponent } from './resources-home/resources-home.component';
 import { ResourcesListComponent } from './resources-list/resources-list.component';
 import { AddResourcesComponent } from './add-resources/add-resources.component';
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { ResourceDetailsComponent } from './resource-details/resource-details.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    component: ResourcesHomeComponent,
-    children: [
-      { path: 'add-resources', component: AddResourcesComponent },
-      { path: 'edit-resources', component: AddResourcesComponent },
-      { path: 'resources-list', component: ResourcesListComponent,
-      children : [
-        {
-            path         : ':id',
-            component    : ResourceDetailsComponent,
-        }
-    ] },
-      {
-        path: "",
-        redirectTo: "resources-list",
-        pathMatch: "full"
-      },
-      {
-        path: "**",
-        redirectTo: "resources-list"
-      }
-    ]
-  }
+    {
+        path: '',
+        component: ResourcesHomeComponent,
+        children: [
+            { path: 'add-resources', component: AddResourcesComponent },
+            { path: 'edit-resources', component: AddResourcesComponent },
+            {
+                path: 'resources-list',
+                component: ResourcesListComponent,
+                data: {
+                    pageTitle: 'Resourses',
+                },
+                children: [
+                    {
+                        path: ':id',
+                        component: ResourceDetailsComponent,
+                    },
+                ],
+            },
+            {
+                path: '',
+                redirectTo: 'resources-list',
+                pathMatch: 'full',
+            },
+            {
+                path: '**',
+                redirectTo: 'resources-list',
+            },
+        ],
+    },
 ];
 
 @NgModule({
-  declarations: [
-    ResourcesHomeComponent,
-    ResourcesListComponent,
-    AddResourcesComponent,
-    ResourceDetailsComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(routes),
-    MatChipsModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    MatMenuModule,
-    MatProgressBarModule,
-    MatTableModule,
-    MatDialogModule,
-    MatDatepickerModule,
-    MatMomentDateModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatRadioModule,
-    MatAutocompleteModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    FuseFindByKeyPipeModule,
-    FuseNavigationModule,
-    FuseScrollbarModule,
-    FuseScrollResetModule,
-    InfiniteScrollModule
-  ],
-  providers: [DatePipe]
+    declarations: [
+        ResourcesHomeComponent,
+        ResourcesListComponent,
+        AddResourcesComponent,
+        ResourceDetailsComponent,
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(routes),
+        MatChipsModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatIconModule,
+        MatMenuModule,
+        MatProgressBarModule,
+        MatTableModule,
+        MatDialogModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatRadioModule,
+        MatAutocompleteModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule,
+        FuseFindByKeyPipeModule,
+        FuseNavigationModule,
+        FuseScrollbarModule,
+        FuseScrollResetModule,
+        InfiniteScrollModule,
+    ],
+    providers: [DatePipe],
 })
-export class ResourcesModule { }
+export class ResourcesModule {}
