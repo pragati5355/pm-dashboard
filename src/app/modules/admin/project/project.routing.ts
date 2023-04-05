052e43e87950d0cca202332e3e696839dec80d15
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-04-05 17:09:28 +0530

    add project routing file

---
+++ src/app/modules/admin/project/project.routing.ts
@@ -0,0 +1,65 @@
+import { Route, Routes } from '@angular/router';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { AddProjectHomeComponent } from '../project/add-project-home/add-project-home.component';
+import { ProjectHomeComponent } from './project-home/project-home.component';
+export const projectRoutes: Route[] = [
+    {
+        path: '',
+        component: ProjectHomeComponent,
+        children: [
+            {
+                path: 'project-list',
+                component: ProjectListComponent,
+                data: {
+                    pageTitle: 'Projects List',
+                },
+            },
+            {
+                path: 'add-project',
+                component: AddProjectHomeComponent,
+                data: {
+                    pageTitle: 'Add New Project',
+                },
+            },
+            {
+                path: 'edit-project',
+                component: AddProjectHomeComponent,
+                data: {
+                    pageTitle: 'Edit Project',
+                },
+            },
+
+            {
+                path: 'project',
+                loadChildren: () =>
+                    import(
+                        '../project/project-widget/project-widget.module'
+                    ).then((m) => m.ProjectWidgetModule),
+            },
+
+            {
+                path: 'repository',
+                loadChildren: () =>
+                    import('../repository/repository.module').then(
+                        (m) => m.RepositoryModule
+                    ),
+            },
+            {
+                path: 'project-process',
+                loadChildren: () =>
+                    import('../project-process/project-process.module').then(
+                        (m) => m.ProjectProcessModule
+                    ),
+            },
+            {
+                path: '',
+                redirectTo: 'project-list',
+                pathMatch: 'full',
+            },
+            {
+                path: '**',
+                redirectTo: 'project-list',
+            },
+        ],
+    },
+];
