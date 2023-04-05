8b4da2222b8166ff4b2967d3e0664b07374ff76e
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-04-05 15:22:16 +0530

    forms router changes

---
+++ src/app/modules/admin/create-form/create-form.routing.ts
@@ -0,0 +1,50 @@
+import { Route, Routes } from '@angular/router';
+import { CreateFormHomeComponent } from './create-form-home/create-form-home.component';
+import { FormListComponent } from './form-list/form-list.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { ViewFormComponent } from './view-form/view-form.component';
+export const createFormRoutes: Route[] = [
+    {
+        path: '',
+        component: CreateFormHomeComponent,
+        children: [
+            {
+                path: 'add',
+                component: AddFormComponent,
+                data: {
+                    pageTitle: 'Add Form',
+                },
+            },
+            {
+                path: 'edit/:id',
+                component: AddFormComponent,
+                data: {
+                    pageTitle: 'Edit Form',
+                },
+            },
+            {
+                path: 'view/:id',
+                component: ViewFormComponent,
+                data: {
+                    pageTitle: 'View Form',
+                },
+            },
+            {
+                path: '',
+                component: FormListComponent,
+                data: {
+                    pageTitle: 'Forms List',
+                },
+            },
+            {
+                path: '',
+                redirectTo: '',
+                pathMatch: 'full',
+            },
+            {
+                path: '**',
+                redirectTo: '',
+            },
+        ],
+    },
+];
--- src/app/modules/admin/project/sprint-feedback-form/sprint-feedback-form.component.ts
+++ src/app/modules/admin/project/sprint-feedback-form/sprint-feedback-form.component.ts
@@ -2,145 +2,146 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AddFormService } from '@services/add-form.service';
 import { AuthService } from '@services/auth/auth.service';
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
-  selector: 'app-sprint-feedback-form',
-  templateUrl: './sprint-feedback-form.component.html',
-  styleUrls: ['./sprint-feedback-form.component.scss']
+    selector: 'app-sprint-feedback-form',
+    templateUrl: './sprint-feedback-form.component.html',
+    styleUrls: ['./sprint-feedback-form.component.scss'],
 })
-export class SprintFeedbackFormComponent  implements OnInit {
-  public form!: Object;
-  formName = ""
-  routeSubscribe: any;
-  initialLoading= false
-  sprintId: any
-  projectId: any
-  emailList:any = []
-  formComponent: any =[]
-  formData: any
-  sprintName: any
-  isShowEmails: boolean = true
-  emailId = 0
-  email: any
-  constructor(private _authService: AuthService,
-    private formService: AddFormService,
-    private _route: ActivatedRoute,
-    private router: Router,
-    public matDialogRef: MatDialogRef<SprintFeedbackFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any ) { }
-  ngOnInit(): void {
-      
-  
-      this._route.queryParams.subscribe((sprintId: any) => {
-        if (sprintId['id'] && sprintId['name']) {
-            this.sprintId = parseInt(sprintId['id'])
-            this.sprintName = sprintId['name']
-        }
+export class SprintFeedbackFormComponent implements OnInit {
+    public form!: Object;
+    formName = '';
+    routeSubscribe: any;
+    initialLoading = false;
+    sprintId: any;
+    projectId: any;
+    emailList: any = [];
+    formComponent: any = [];
+    formData: any;
+    sprintName: any;
+    isShowEmails: boolean = true;
+    emailId = 0;
+    email: any;
+    constructor(
+        private _authService: AuthService,
+        private formService: AddFormService,
+        private _route: ActivatedRoute,
+        private router: Router,
+        public matDialogRef: MatDialogRef<SprintFeedbackFormComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) {}
+    ngOnInit(): void {
+        this._route.queryParams.subscribe((sprintId: any) => {
+            if (sprintId['id'] && sprintId['name']) {
+                this.sprintId = parseInt(sprintId['id']);
+                this.sprintName = sprintId['name'];
+            }
         });
-        let projectData= this._authService.getProjectDetails()
-        this.projectId = projectData.id
-          this.getFeedbackFormEmailList()
-  }
-   getFormDetailsBySprint(){
-    this.initialLoading = true;
-      let payload = {
-        projectId: this.projectId,
-        sprintId: this.sprintId
-      }
-     this.formService.getFeedbackFormBySprint(payload).subscribe((res: any) =>{
+        let projectData = this._authService.getProjectDetails();
+        this.projectId = projectData.id;
+        this.getFeedbackFormEmailList();
+    }
+    getFormDetailsBySprint() {
+        this.initialLoading = true;
+        let payload = {
+            projectId: this.projectId,
+            sprintId: this.sprintId,
+        };
+        this.formService
+            .getFeedbackFormBySprint(payload)
+            .subscribe((res: any) => {
+                this.initialLoading = false;
+                let formdata: any = [];
+                formdata.push(res.data.form);
+                formdata.forEach((item: any) => {
+                    this.formName = item.formName;
+                    this.formComponent = item.formComponent.components;
+                    this.formComponent = this.formComponent.filter(
+                        (element: any) => element.key !== 'submit'
+                    );
+                });
+                this.form = { components: this.formComponent };
+            });
+    }
+    getFeedbackFormEmailList() {
+        this.initialLoading = true;
+        let payload = {
+            projectId: this.projectId,
+            sprintId: this.sprintId,
+        };
+        this.formService
+            .getFeedbackFormEamilList(payload)
+            .subscribe((res: any) => {
+                this.initialLoading = false;
+                this.emailList = res.data;
+                this.getFormDetailsBySprint();
+                this.getFormDetailsByEmail(0);
+            });
+    }
+    getFormDetailsByEmail(index: number) {
+        this.initialLoading = true;
+        this.emailId = index;
+        let payload = {
+            projectId: this.projectId,
+            sprintId: this.sprintId,
+            email: this.emailList[index],
+        };
+        this.email = this.emailList[index];
+        this.formService
+            .getFeedbackFormByEmail(payload)
+            .subscribe((res: any) => {
+                this.initialLoading = false;
+                this.data = res.data.formResponse;
+                this.formData = { data: res.data.formResponse };
+                this.isShowEmails = false;
+            });
+    }
+    getFormDetailsByEmailPrevious() {
+        if (this.emailId > 0) {
+            this.emailId = this.emailId - 1;
 
-      this.initialLoading = false;
-      let formdata: any = []
-      formdata.push(res.data.form)
-      formdata.forEach((item: any) => {
-        this.formName = item.formName
-        this.formComponent = item.formComponent.components
-        this.formComponent = this.formComponent.filter((element: any)=> element.key !== 'submit')
-       
-      });
-      this.form = {components: this.formComponent}
-     
-     })
-   }
-   getFeedbackFormEmailList(){
-    this.initialLoading = true;
-      let payload = {
-        projectId: this.projectId,
-        sprintId: this.sprintId
-      }
-     this.formService.getFeedbackFormEamilList(payload).subscribe((res: any) =>{
-
-      this.initialLoading = false
-      this.emailList = res.data
-      this.getFormDetailsBySprint()
-      this.getFormDetailsByEmail(0);
-     })
-   }
-   getFormDetailsByEmail(index: number){
-     
-    this.initialLoading = true;
-    this.emailId = index
-      let payload = {
-        projectId: this.projectId,
-        sprintId: this.sprintId,
-        email:this.emailList[index]
-      }
-      this.email=this.emailList[index]
-     this.formService.getFeedbackFormByEmail(payload).subscribe((res: any) =>{
-
-      this.initialLoading = false;
-      this.data = res.data.formResponse
-      this.formData={data:res.data.formResponse}
-      this.isShowEmails = false
-     })
-   }
-   getFormDetailsByEmailPrevious(){
-    if(this.emailId > 0){
-        this.emailId = this.emailId -1
-
-   this.initialLoading = true;
-     let payload = {
-       projectId: this.projectId,
-       sprintId: this.sprintId,
-       email:this.emailList[this.emailId]
-     }
-     this.email=this.emailList[this.emailId]
-    this.formService.getFeedbackFormByEmail(payload).subscribe((res: any) =>{
-
-     this.initialLoading = false;
-     this.data = res.data.formResponse
-     this.formData={data:res.data.formResponse}
-     this.isShowEmails = false
-    })
-  }
-  }
-  getFormDetailsByEmailNext(){
-   if(this.emailId < this.emailList.length-1){
-    this.emailId = this.emailId +1
-    this.initialLoading = true;
-      let payload = {
-        projectId: this.projectId,
-        sprintId: this.sprintId,
-        email:this.emailList[this.emailId]
-      }
-      this.email=this.emailList[this.emailId]
-     this.formService.getFeedbackFormByEmail(payload).subscribe((res: any) =>{
- 
-      this.initialLoading = false;
-      this.data = res.data.formResponse
-      this.formData={data:res.data.formResponse}
-      this.isShowEmails = false
-     })
-   }
-  }
-   submit(event: any) {
-  }
-  gotoforms(){
-    this.router.navigate(
-        [`/forms/form-list`]
-    );
-}
-close(){
-  this.matDialogRef.close();
-}
+            this.initialLoading = true;
+            let payload = {
+                projectId: this.projectId,
+                sprintId: this.sprintId,
+                email: this.emailList[this.emailId],
+            };
+            this.email = this.emailList[this.emailId];
+            this.formService
+                .getFeedbackFormByEmail(payload)
+                .subscribe((res: any) => {
+                    this.initialLoading = false;
+                    this.data = res.data.formResponse;
+                    this.formData = { data: res.data.formResponse };
+                    this.isShowEmails = false;
+                });
+        }
+    }
+    getFormDetailsByEmailNext() {
+        if (this.emailId < this.emailList.length - 1) {
+            this.emailId = this.emailId + 1;
+            this.initialLoading = true;
+            let payload = {
+                projectId: this.projectId,
+                sprintId: this.sprintId,
+                email: this.emailList[this.emailId],
+            };
+            this.email = this.emailList[this.emailId];
+            this.formService
+                .getFeedbackFormByEmail(payload)
+                .subscribe((res: any) => {
+                    this.initialLoading = false;
+                    this.data = res.data.formResponse;
+                    this.formData = { data: res.data.formResponse };
+                    this.isShowEmails = false;
+                });
+        }
+    }
+    submit(event: any) {}
+    gotoforms() {
+        this.router.navigate([`/forms/`]);
+    }
+    close() {
+        this.matDialogRef.close();
+    }
 }
