5ffb0eb4ec8fb3daec7450788708b2ed77bb8ea3
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-04-17 16:07:26 +0530

    remove console

--- src/app/modules/admin/repository/add-repository/add-repository.component.ts
+++ src/app/modules/admin/repository/add-repository/add-repository.component.ts
@@ -483,7 +483,7 @@ export class AddRepositoryComponent implements OnInit {
                 }
                 link.click();
             })
-            .catch(console.error);
+            .catch();
     }
 
     sendEmail() {
