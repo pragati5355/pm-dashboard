6a674f53ecf1d77813d4be33e2e7c3f4bc54586e
Sanskriti Gupta <sanskriti.gupta@mindbowser.com>
2023-04-19 16:14:57 +0530

    success message changes

--- src/app/core/utils/snackBar.ts
+++ src/app/core/utils/snackBar.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar";
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 @Injectable({
-    providedIn: 'root'
-  })
+    providedIn: 'root',
+})
 export class SnackBar {
-  constructor(private _snackBar: MatSnackBar) {}
-  config = new MatSnackBarConfig();
-  successSnackBar(text: string): void {
-    this.config.duration = 5000;
-    this.config.panelClass = ["success-snackbar"];
-    this.config.horizontalPosition = "right";
-    this.config.verticalPosition = "bottom";
-    this._snackBar.open(text, "X", this.config);
-  }
-  errorSnackBar(text: string): void {
-    this.config.duration = 5000;
-    this.config.panelClass = ["red-snackbar"];
-    this.config.horizontalPosition = "right";
-    this.config.verticalPosition = "bottom";
-    this._snackBar.open(text, "X", this.config);
-  }
+    constructor(private _snackBar: MatSnackBar) {}
+    config = new MatSnackBarConfig();
+    successSnackBar(text: string): void {
+        this.config.duration = 5000;
+        this.config.panelClass = ['success-snackbar'];
+        this.config.horizontalPosition = 'right';
+        this.config.verticalPosition = 'bottom';
+        this._snackBar.open('😃' + text, 'X', this.config);
+    }
+    errorSnackBar(text: string): void {
+        this.config.duration = 5000;
+        this.config.panelClass = ['red-snackbar'];
+        this.config.horizontalPosition = 'right';
+        this.config.verticalPosition = 'bottom';
+        this._snackBar.open('😔' + text, 'X', this.config);
+    }
 }
