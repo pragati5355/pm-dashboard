34b02aaf28bbf74b9165144b1344431c05ae872d
Your Name <you@example.com>
2023-05-22 15:58:18 +0530

    added the reports ui with api integration

--- src/app/modules/admin/dashboard/common/services/dashboard-api.service.ts
+++ src/app/modules/admin/dashboard/common/services/dashboard-api.service.ts
@@ -7,9 +7,17 @@ import { AppConstants } from 'app/core/constacts/constacts';
 export class DashboardApiService {
     downloadExcelUrl =
         AppConstants['PROJECT_API_URL'] + '/resource-utilization-report';
+
+    downloadExcelAvailabilityUrl = 
+        AppConstants['PROJECT_API_URL'] + '/resource-report';
+
     constructor(private http: HttpClient) {}
 
     getUtilisationExcelReport() {
         return this.http.get(this.downloadExcelUrl);
+    }
+
+    getAvailabilityExcelReport(){
+        return this.http.get(this.downloadExcelAvailabilityUrl);
     }
 }
