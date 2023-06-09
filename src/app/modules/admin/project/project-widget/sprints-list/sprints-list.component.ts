91b8aa4cc183155f7d3324580a1bdd9386f47f6b
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-06-09 17:54:18 +0530

    mark sprint as completed in sprint list

--- src/app/modules/admin/project/project-widget/sprints-list/sprints-list.component.ts
+++ src/app/modules/admin/project/project-widget/sprints-list/sprints-list.component.ts
@@ -6,6 +6,10 @@ import { Input } from '@angular/core';
 import { AuthService } from '@services/auth/auth.service';
 import { InvoicePercentageComponent } from '../invoice-percentage/invoice-percentage.component';
 import { MatDialog } from '@angular/material/dialog';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SprintService } from '../../common/services/sprint.service';
+import { SnackBar } from 'app/core/utils/snackBar';
 @Component({
     selector: 'app-sprints-list',
     templateUrl: './sprints-list.component.html',
@@ -17,6 +21,7 @@ export class SprintsListComponent implements OnInit {
     totalRecored = 0;
     totalPerPageData = StaticData.PER_PAGE_DATA;
     sprintList: any = [];
+    configForm: FormGroup;
     requiredSprintSkeletonData = {
         rowsToDisplay: 10,
         displayProfilePicture: false,
@@ -26,7 +31,11 @@ export class SprintsListComponent implements OnInit {
         private _authService: AuthService,
         private ProjectService: CreateProjecteService,
         private router: Router,
-        private dialog: MatDialog
+        private dialog: MatDialog,
+        private fuseConfirmationService: FuseConfirmationService,
+        private formBuilder: FormBuilder,
+        private sprintService: SprintService,
+        private snackBar: SnackBar
     ) {}
     @Input() dataId: any;
     ngOnInit(): void {
@@ -34,6 +43,7 @@ export class SprintsListComponent implements OnInit {
         let payload = {
             id: this.dataId,
         };
+        this.initializeConfigForm();
         this.getSprintList(payload);
     }
 
@@ -61,9 +71,43 @@ export class SprintsListComponent implements OnInit {
         );
     }
     goToSprint(id: number) {
-        this.router.navigate([
-            `/projects/${this.dataId}/sprint-details/${id}`,
-        ]);
+        this.router.navigate([`/projects/${this.dataId}/sprint-details/${id}`]);
+    }
+    markAsComplete(sprintId) {
+        const payload = {
+            id: sprintId,
+            status: 'COMPLETED',
+        };
+        const dialogRef = this.fuseConfirmationService.open(
+            this.configForm.value
+        );
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result == 'confirmed') {
+                this.updateSprintStatus(payload);
+            }
+        });
+    }
+    updateSprintStatus(payload: any) {
+        this.initialLoading = true;
+        this.sprintService.postSprintStatus(payload).subscribe(
+            (res: any) => {
+                if (!res?.error) {
+                    this.initialLoading = false;
+                    this.snackBar.successSnackBar(res?.message);
+                } else {
+                    this.initialLoading = false;
+                    this.snackBar.errorSnackBar(res?.message);
+                }
+                if (res?.tokenExpire == true) {
+                    this._authService.updateAndReload(window.location);
+                }
+            },
+            (err: any) => {
+                this.initialLoading = false;
+                this.snackBar.errorSnackBar('Something Went Wrong');
+            }
+        );
     }
     openInvoiceDialog(data: any) {
         const dialogRef = this.dialog.open(InvoicePercentageComponent, {
@@ -82,6 +126,30 @@ export class SprintsListComponent implements OnInit {
                 };
                 this.getSprintList(payload);
             }
+        });
+    }
+    private initializeConfigForm() {
+        this.configForm = this.formBuilder.group({
+            title: 'Mark this sprint as Complete?',
+            message:
+                'Are you sure you want to mark this sprint as completed ? <br/> <span class="font-medium">This action will trigger the mail to the admin.</span>',
+            icon: this.formBuilder.group({
+                show: true,
+                name: 'heroicons_outline:exclamation',
+                color: 'primary',
+            }),
+            actions: this.formBuilder.group({
+                confirm: this.formBuilder.group({
+                    show: true,
+                    label: 'Yes',
+                    color: 'primary',
+                }),
+                cancel: this.formBuilder.group({
+                    show: true,
+                    label: 'No',
+                }),
+            }),
+            dismissible: false,
         });
     }
 }
