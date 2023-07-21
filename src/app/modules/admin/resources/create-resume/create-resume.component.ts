41246c71d690bd1de1825e51c9b048d9e3fad8a2
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-07-21 12:44:27 +0530

    fuse classes overwrite

--- src/app/modules/admin/resources/create-resume/create-resume.component.ts
+++ src/app/modules/admin/resources/create-resume/create-resume.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-create-resume',
-  templateUrl: './create-resume.component.html',
-  styleUrls: ['./create-resume.component.scss']
+    selector: 'app-create-resume',
+    templateUrl: './create-resume.component.html',
+    styleUrls: ['./create-resume.component.scss'],
 })
 export class CreateResumeComponent implements OnInit {
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private _formBuilder: FormBuilder,
+        private matDialogRef: MatDialogRef<CreateResumeComponent>
+    ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
+    cancel() {
+        this.matDialogRef.close();
+    }
 }
