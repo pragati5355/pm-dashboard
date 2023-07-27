33c1145456ca6737557c4f1b1cc3cd0e18191955
Pragati B Gawade <pragati.gawade@mindbowser.com>
2023-07-27 10:57:22 +0530

    Integrated the registered resource

--- src/app/modules/admin/resources/onboard-resource-details/onboard-resource-details.component.ts
+++ src/app/modules/admin/resources/onboard-resource-details/onboard-resource-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,15 +9,80 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class OnboardResourceDetailsComponent implements OnInit {
     initialLoading = false;
+    resourceForm: FormGroup;
+    mode: string;
+    patchData: [] | null;
+    submitInProcess = false;
+    disableField: boolean = false;
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: any,
+        private _formBuilder: FormBuilder,
         public matDialogRef: MatDialogRef<OnboardResourceDetailsComponent>
     ) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.loadData();
+        this.initializeForm();
+    }
+
+    private loadData() {
+        this.mode = this.data?.mode;
+        this.patchData = this.data?.editData;
+
+        console.log('this.mode -> ', this.mode);
+        console.log('this.patchData -> ', this.patchData);
+    }
 
     close() {
         this.matDialogRef.close();
+    }
+
+    submitResourceData() {
+        if (this.mode === 'VIEW') {
+            this.matDialogRef.close();
+        }
+    }
+
+    private patchValuesInEditMode() {
+        if (this.mode === 'VIEW') {
+            this.resourceForm.patchValue({
+                email: this.data?.editData?.email,
+                role: this.data?.editData?.role,
+                firstName: this.data?.editData?.details?.firstName,
+                lastName: this.data?.editData?.details?.lastName,
+            });
+        }
+        if (this.mode === 'EDIT') {
+            this.resourceForm.patchValue({
+                email: this.data?.editData?.email,
+                role: this.data?.editData?.role,
+                firstName: this.data?.editData?.details?.firstName,
+                lastName: this.data?.editData?.details?.lastName,
+            });
+        }
+    }
+
+    private initializeForm() {
+        this.resourceForm = this._formBuilder.group({
+            firstName: ['', [Validators.required]],
+            lastName: ['', [Validators.required]],
+            email: [
+                '',
+                [
+                    Validators.required,
+                    Validators.email,
+                    Validators.pattern(/@mindbowser.com\s*$/),
+                ],
+            ],
+            role: ['', [Validators.required]],
+        });
+
+        this.patchValuesInEditMode();
+        if (this.mode === 'VIEW') {
+            this.resourceForm.get('email').disable();
+            this.resourceForm.get('firstName').disable();
+            this.resourceForm.get('lastName').disable();
+        }
     }
 }
--- src/app/modules/admin/resources/onboard-resource/onboard-resource.component.ts
+++ src/app/modules/admin/resources/onboard-resource/onboard-resource.component.ts
@@ -1,7 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+    MAT_DIALOG_DATA,
+    MatDialog,
+    MatDialogRef,
+} from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { OnboardResourceDetailsComponent } from '../onboard-resource-details/onboard-resource-details.component';
+import { AuthService } from '@services/auth/auth.service';
+import { ResourceService } from '@modules/public/resource/common/services/resource.service';
+import { ResourcesService } from '../common/services/resources.service';
 
 @Component({
     selector: 'app-onboard-resource',
@@ -10,36 +17,67 @@ import { OnboardResourceDetailsComponent } from '../onboard-resource-details/onb
 })
 export class OnboardResourceComponent implements OnInit {
     initialLoading = false;
+    registeredList: any = [];
+    perPageData = 1;
+    totalPerPageData = 10;
+    totalRecord: any;
     requiredReposSkeletonData = {
         rowsToDisplay: 10,
         displayProfilePicture: false,
     };
 
     constructor(
-      private router: Router,
-      private dialog: MatDialog,
-      ) {}
+        private router: Router,
+        private dialog: MatDialog,
+        private _authService: AuthService,
+        private resourceService: ResourcesService
+    ) {}
 
-    ngOnInit(): void {}
+    ngOnInit() {
+        this.getList();
+    }
+
+    getList() {
+        const payload = {
+            perPageData: this.perPageData,
+            totalPerPageData: this.totalPerPageData,
+        };
+        this.initialLoading = true;
+        this.resourceService
+            .getRegisteredResource(payload)
+            .subscribe((res: any) => {
+                this.initialLoading = false;
+                if (!res?.error) {
+                    this.registeredList = res?.data?.resource;
+                    this.totalRecord = res?.data?.totalRecored;
+                    console.log('this.totalRecord -> ', this.totalRecord);
+                    console.log('this.registeredList -> ', this.registeredList);
+                }
+                if (res?.tokenExpire) {
+                    this._authService.updateAndReload(window.location);
+                }
+            });
+    }
 
     goBack() {
         this.router.navigate(['/resources']);
     }
 
-    gotoDetailspage(){
-      const dialogRef = this.dialog.open(OnboardResourceDetailsComponent, {
-        disableClose: true,
-        width: '50%',
-        panelClass: 'warn-dialog-content',
-        autoFocus: false,
-        data: {
-  
-        },
-    });
-      dialogRef.afterClosed().subscribe((result: any) => {
-        if (result == 'success') {
-           
-        }
-      });
+    gotoDetailspage(mode: String, data: any) {
+        const dialogRef = this.dialog.open(OnboardResourceDetailsComponent, {
+            disableClose: true,
+            width: '50%',
+            panelClass: 'warn-dialog-content',
+            autoFocus: false,
+            data: {
+                mode: mode,
+                editData: data,
+            },
+        });
+        dialogRef.afterClosed().subscribe((result: any) => {
+            if (result == 'success') {
+                this.getList();
+            }
+        });
     }
 }
