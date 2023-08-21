b6c084fcee522016171d4f753d817513f8f66c9c
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-08-21 13:47:36 +0530

    removed on bench in edit project

--- src/app/modules/admin/external-projects/external-project-details/external-project-details.component.ts
+++ src/app/modules/admin/external-projects/external-project-details/external-project-details.component.ts
@@ -176,6 +176,7 @@ export class ExternalProjectDetailsComponent implements OnInit {
             {
                 disableClose: true,
                 width: '50%',
+                maxHeight:'90vh',
                 panelClass: 'warn-dialog-content',
                 autoFocus: false,
                 data: {
