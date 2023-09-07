acd4e4c7a2a8f77f8ff495765af0700fdbbc1778
Pragati B Gawade <pragati.gawade@mindbowser.com>
2023-09-07 12:51:08 +0530

    Added the search Ui for project list

--- src/app/modules/admin/profit-loss/profit-loss-projects-list/profit-loss-projects-list.component.ts
+++ src/app/modules/admin/profit-loss/profit-loss-projects-list/profit-loss-projects-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profit-loss-projects-list',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfitLossProjectsListComponent implements OnInit {
 
+  projectList = [];
+  filteredProjectList = [];
+  searchControl = new FormControl();
+
+  initialLoading: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addSearchListener() {
+    this.searchControl?.valueChanges.subscribe((searchKey: string) => {
+        searchKey = searchKey?.trim()?.toLowerCase();
+        if (searchKey) {
+            this.filteredProjectList = this.projectList.filter(
+                (project) =>
+                    project?.name?.toLowerCase()?.includes(searchKey) ||
+                    project?.description?.toLowerCase()?.includes(searchKey)
+            );
+        } else {
+            this.filteredProjectList = this.projectList;
+        }
+    });
+  }
+
+  // private loadExternalProjectsList() {
+  //   this.initialLoading = true;
+  //   this.externalProjectsService.getExternalProjectsList().subscribe(
+  //       (res: any) => {
+  //           this.initialLoading = false;
+  //           if (res?.error === false) {
+  //               this.projectList = res?.data;
+  //               this.filteredProjectList = res?.data;
+  //           }
+  //           if (res?.tokenExpire) {
+  //               this._authService.updateAndReload(window.location);
+  //           }
+  //       },
+  //       (err) => {
+  //           this.initialLoading = false;
+  //       }
+  //   );
+  // }
+
 
 }
--- src/app/modules/admin/profit-loss/profit-loss.module.ts
+++ src/app/modules/admin/profit-loss/profit-loss.module.ts
@@ -4,16 +4,70 @@ import { CommonModule } from '@angular/common';
 import { ProfitLossRoutingModule } from './profit-loss-routing.module';
 import { ProfitLossHomeComponent } from './profit-loss-home/profit-loss-home.component';
 import { ProfitLossProjectsListComponent } from './profit-loss-projects-list/profit-loss-projects-list.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FuseFindByKeyPipeModule } from '@fuse/pipes/find-by-key';
+import { FuseNavigationModule } from '@fuse/components/navigation';
+import { FuseScrollResetModule } from '@fuse/directives/scroll-reset';
+import { FuseScrollbarModule } from '@fuse/directives/scrollbar';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FuseCardModule } from '@fuse/components/card';
+import { SkeletonModule } from 'app/core/modules/skeleton/skeleton.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTabsModule } from '@angular/material/tabs';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
-  declarations: [
-    ProfitLossHomeComponent,
-    ProfitLossProjectsListComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfitLossRoutingModule
-  ]
+    declarations: [
+      ProfitLossHomeComponent, 
+      ProfitLossProjectsListComponent,
+    ],
+    imports: [
+        CommonModule,
+        ProfitLossRoutingModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatMenuModule,
+        MatProgressBarModule,
+        MatTableModule,
+        MatDialogModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatRadioModule,
+        MatAutocompleteModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule,
+        FuseFindByKeyPipeModule,
+        FuseNavigationModule,
+        FuseScrollbarModule,
+        FuseScrollResetModule,
+        InfiniteScrollModule,
+        MatStepperModule,
+        FuseCardModule,
+        SkeletonModule,
+        FormsModule,
+        MatCheckboxModule,
+        MatTabsModule,
+        QuillModule,
+    ],
 })
-export class ProfitLossModule { }
+export class ProfitLossModule {}
