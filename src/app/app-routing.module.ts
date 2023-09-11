33a9c69a20f79a303aa0d68b2ebed0f342c88c8c
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-09-11 16:05:04 +0530

    public route created for project work log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { NoAuthGuard } from '@services/auth/guards/noAuth.guard';
 import { LayoutComponent } from './layout/layout.component';
 import { WorkLogsComponent } from '@modules/public/shared/work-logs/work-logs.component';
 import { WrongUrlComponent } from '@modules/public/shared/wrong-url/wrong-url.component';
+import { WorkLogsListComponent } from '@modules/public/shared/work-logs-list/work-logs-list.component';
 export const appRoutes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
 
@@ -107,6 +108,7 @@ export const appRoutes: Routes = [
         path: 'wrong-url',
         component: WrongUrlComponent,
     },
+    { path: 'project/:key', component: WorkLogsListComponent },
     {
         path: 'resource',
         loadChildren: () =>
---
+++ src/app/modules/public/shared/work-logs-list/work-logs-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-logs-list',
+  templateUrl: './work-logs-list.component.html',
+  styleUrls: ['./work-logs-list.component.scss']
+})
+export class WorkLogsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
