9fcd6af7752c679bca5d052c22a47d66f813d019
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-09-12 21:58:25 +0530

    work log list ui fixed

--- src/app/modules/admin/common/services/logged-in-user.service.ts
+++ src/app/modules/admin/common/services/logged-in-user.service.ts
@@ -9,16 +9,7 @@ import { BehaviorSubject, map, Observable, of } from 'rxjs';
     providedIn: 'root',
 })
 export class LoggedInUserService {
-    private loggedInUser: BehaviorSubject<any> = new BehaviorSubject({
-        id: 3,
-        email: 'rohan.kadam@mindbowser.com',
-        firstName: 'Rohan',
-        lastName: 'Kadam',
-        status: 'ACTIVATED',
-        role: 'ADMIN',
-        resourceId: 1,
-        deleted: false,
-    });
+    private loggedInUser: BehaviorSubject<any> = new BehaviorSubject(null);
     private userData: any = null;
 
     constructor(private http: HttpClient, private authService: AuthService) {}
@@ -40,16 +31,7 @@ export class LoggedInUserService {
         return this.http.get(API_LIST.LOGGED_IN_USER).pipe(
             map((response: any) => {
                 if (response && response['data']) {
-                    this.setUser({
-                        id: 3,
-                        email: 'rohan.kadam@mindbowser.com',
-                        firstName: 'Rohan',
-                        lastName: 'Kadam',
-                        status: 'ACTIVATED',
-                        role: 'ADMIN',
-                        resourceId: 1,
-                        deleted: false,
-                    });
+                    this.setUser(response['data']);
                     return response['data'];
                 }
                 return null;
