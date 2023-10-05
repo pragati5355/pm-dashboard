125683c89516a52b1d80d3707ce9748193807bd3
Pragati B Gawade <pragati.gawade@mindbowser.com>
2023-10-05 22:13:29 +0530

    added the profit or loss statement

--- src/app/modules/admin/profit-loss/profit-loss-project-statistic/profit-loss-project-statistic.component.ts
+++ src/app/modules/admin/profit-loss/profit-loss-project-statistic/profit-loss-project-statistic.component.ts
@@ -155,85 +155,85 @@ export class ProfitLossProjectStatisticComponent implements OnInit {
         this.previousMonth = new Date().getMonth() - 1;
     }
 
-    private filterTotalOfAllColumns() {
-        /* Total Ideal Work Hrs */
-        this.statList.forEach((data) => {
-            this.idealWorkHrs = data?.totalIdealWorklogHrs;
-            this.addIdealWorkHrs.push(this.idealWorkHrs);
-        });
-        let resultIdealWorkHrs = 0;
-        this.addIdealWorkHrs.forEach((number) => {
-            resultIdealWorkHrs += number;
-        });
-        this.sumIdealWorkHrs = resultIdealWorkHrs;
+    // private filterTotalOfAllColumns() {
+    //     /* Total Ideal Work Hrs */
+    //     this.statList.forEach((data) => {
+    //         this.idealWorkHrs = data?.totalIdealWorklogHrs;
+    //         this.addIdealWorkHrs.push(this.idealWorkHrs);
+    //     });
+    //     let resultIdealWorkHrs = 0;
+    //     this.addIdealWorkHrs.forEach((number) => {
+    //         resultIdealWorkHrs += number;
+    //     });
+    //     this.sumIdealWorkHrs = resultIdealWorkHrs;
 
-        /* Total Actual Work Hrs */
-        this.statList.forEach((data) => {
-            this.actualWorkHrs = data?.totalActualWorklogHrs;
-            this.addActualWorkHrs.push(this.actualWorkHrs);
-        });
-        let resultActualWorkHrs = 0;
-        this.addActualWorkHrs.forEach((number) => {
-            resultActualWorkHrs += number;
-        });
-        this.sumActualWorkHrs = resultActualWorkHrs;
+    //     /* Total Actual Work Hrs */
+    //     this.statList.forEach((data) => {
+    //         this.actualWorkHrs = data?.totalActualWorklogHrs;
+    //         this.addActualWorkHrs.push(this.actualWorkHrs);
+    //     });
+    //     let resultActualWorkHrs = 0;
+    //     this.addActualWorkHrs.forEach((number) => {
+    //         resultActualWorkHrs += number;
+    //     });
+    //     this.sumActualWorkHrs = resultActualWorkHrs;
 
-         /* Total Resource Cost */
-        this.statList.forEach((data) => {
-            this.resourceCost = data?.idealResourceCost;
-            this.addResourceCost.push(this.resourceCost);
-        });
-        let resultResourceCost = 0;
-        this.addResourceCost.forEach((number) => {
-            resultResourceCost += number;
-        });
-        this.sumResourceCost = resultResourceCost;
+    //      /* Total Resource Cost */
+    //     this.statList.forEach((data) => {
+    //         this.resourceCost = data?.idealResourceCost;
+    //         this.addResourceCost.push(this.resourceCost);
+    //     });
+    //     let resultResourceCost = 0;
+    //     this.addResourceCost.forEach((number) => {
+    //         resultResourceCost += number;
+    //     });
+    //     this.sumResourceCost = resultResourceCost;
 
-        /* Total Project Cost */
-        this.statList.forEach((data) => {
-            this.projectCost = data?.idealProjectCost;
-            this.addProjectCost.push(this.projectCost);
-        });
-        let resultProjectCost = 0;
-        this.addProjectCost.forEach((number) => {
-            resultProjectCost += number;
-        });
-        this.sumProjectCost = resultProjectCost;
+    //     /* Total Project Cost */
+    //     this.statList.forEach((data) => {
+    //         this.projectCost = data?.idealProjectCost;
+    //         this.addProjectCost.push(this.projectCost);
+    //     });
+    //     let resultProjectCost = 0;
+    //     this.addProjectCost.forEach((number) => {
+    //         resultProjectCost += number;
+    //     });
+    //     this.sumProjectCost = resultProjectCost;
         
 
-        /* Total Cost on Project */
-        this.statList.forEach((data) => {
-            this.idealCost = data?.costOnProject;
-            this.addIdealCost.push(this.idealCost);
-        });
-        let resultIdealCost = 0;
-        this.addIdealCost.forEach((number) => {
-            resultIdealCost += number;
-        });
-        this.sumIdealCost = resultIdealCost;
+    //     /* Total Cost on Project */
+    //     this.statList.forEach((data) => {
+    //         this.idealCost = data?.costOnProject;
+    //         this.addIdealCost.push(this.idealCost);
+    //     });
+    //     let resultIdealCost = 0;
+    //     this.addIdealCost.forEach((number) => {
+    //         resultIdealCost += number;
+    //     });
+    //     this.sumIdealCost = resultIdealCost;
 
-        /* Total Actual Cost */
-        this.statList.forEach((data) => {
-            this.actualCost = data?.actualCost;
-            this.addActualCost.push(this.actualCost);
-        });
-        let result = 0;
-        this.addActualCost.forEach((number) => {
-            result += number;
-        });
-        this.sumActualCost = result;
+    //     /* Total Actual Cost */
+    //     this.statList.forEach((data) => {
+    //         this.actualCost = data?.actualCost;
+    //         this.addActualCost.push(this.actualCost);
+    //     });
+    //     let result = 0;
+    //     this.addActualCost.forEach((number) => {
+    //         result += number;
+    //     });
+    //     this.sumActualCost = result;
 
-        /* Total Difference */
-        this.statList.forEach((data) => {
-            this.difference = data?.diff;
-            this.adddifference.push(this.difference);
-        });
-        let resultdiff = 0;
-        this.adddifference.forEach((number) => {
-            resultdiff += number;
-        });
-        this.sumdifference = resultdiff;
-    }
+    //     /* Total Difference */
+    //     this.statList.forEach((data) => {
+    //         this.difference = data?.diff;
+    //         this.adddifference.push(this.difference);
+    //     });
+    //     let resultdiff = 0;
+    //     this.adddifference.forEach((number) => {
+    //         resultdiff += number;
+    //     });
+    //     this.sumdifference = resultdiff;
+    // }
 
     private routeSubscribeId() {
         this.routeSubscribe = this._route.params.subscribe((id) => {
@@ -251,7 +251,6 @@ export class ProfitLossProjectStatisticComponent implements OnInit {
                 this.initialLoading = false;
                 if (res?.statusCode === 200) {
                     this.statList = res?.data;
-                    this.filterTotalOfAllColumns();
                 } else if (res?.data == null) {
                     this.statList = [];
                 }
