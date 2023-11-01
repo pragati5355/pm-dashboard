6d7943e9bfc0b12321cf08c6a4abd448a1767e44
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-11-01 12:58:05 +0530

    mentee list view

--- src/app/modules/admin/mentee/mentee-list/mentee-list.component.ts
+++ src/app/modules/admin/mentee/mentee-list/mentee-list.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggedInUserService } from '@modules/admin/common/services/logged-in-user.service';
 
 @Component({
-  selector: 'app-mentee-list',
-  templateUrl: './mentee-list.component.html',
-  styleUrls: ['./mentee-list.component.scss']
+    selector: 'app-mentee-list',
+    templateUrl: './mentee-list.component.html',
+    styleUrls: ['./mentee-list.component.scss'],
 })
 export class MenteeListComponent implements OnInit {
+    userRole: string = '';
+    loggedInUserId: number;
+    initialLoading: boolean = false;
+    menteeList: any[] = [
+        {
+            name: 'Rohan kadam',
+            email: 'rohan@mindbowser.com',
+            team: 'FRONTEND',
+            technologies: ['Angular', 'CSS'],
+        },
+        {
+            name: 'Amaresh joshi',
+            email: 'amaresh@mindbowser.com',
+            team: 'BACKEND',
+            technologies: ['Angular', 'CSS', 'JAVA', 'PYTHON'],
+        },
+        {
+            name: 'Pragati gawade',
+            email: 'pragati@mindbowser.com',
+            team: 'FRONTEND',
+            technologies: [
+                'Angular',
+                'CSS',
+                'Angular',
+                'CSS',
+                'JAVA',
+                'PYTHON',
+            ],
+        },
+    ];
+    constructor(private loggedInUserService: LoggedInUserService) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.getUserRole();
+    }
 
-  ngOnInit(): void {
-  }
-
+    private getUserRole() {
+        this.loggedInUserService.getLoggedInUser().subscribe((res: any) => {
+            if (res?.role) {
+                this.userRole = res?.role;
+                this.loggedInUserId = res?.resourceId;
+                console.log(this.loggedInUserId);
+            }
+        });
+    }
 }
--- src/app/modules/admin/mentee/mentee.module.ts
+++ src/app/modules/admin/mentee/mentee.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MenteeRoutingModule } from './mentee-routing.module';
 import { MenteeHomeComponent } from './mentee-home/mentee-home.component';
 import { MenteeListComponent } from './mentee-list/mentee-list.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    MenteeHomeComponent,
-    MenteeListComponent
-  ],
-  imports: [
-    CommonModule,
-    MenteeRoutingModule
-  ]
+    declarations: [MenteeHomeComponent, MenteeListComponent],
+    imports: [
+        CommonModule,
+        MenteeRoutingModule,
+        MatIconModule,
+        MatButtonModule,
+        MatTooltipModule,
+    ],
 })
-export class MenteeModule { }
+export class MenteeModule {}
