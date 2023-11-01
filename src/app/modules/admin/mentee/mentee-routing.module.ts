a3e529cbff2714249c8c4ad4af19ed59206289e1
Rohan Kadam <rohan.kadam@mindbowser.com>
2023-11-01 12:20:48 +0530

    component setup for mentee

--- src/app/modules/admin/mentee/mentee-routing.module.ts
+++ src/app/modules/admin/mentee/mentee-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenteeHomeComponent } from './mentee-home/mentee-home.component';
+import { MenteeListComponent } from './mentee-list/mentee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: MenteeHomeComponent,
+        children: [
+            {
+                path: '',
+                component: MenteeListComponent,
+                data: {
+                    pageTitle: 'Mentee',
+                },
+            },
+            {
+                path: '',
+                redirectTo: '',
+                pathMatch: 'full',
+            },
+            {
+                path: '**',
+                redirectTo: '',
+            },
+        ],
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class MenteeRoutingModule { }
+export class MenteeRoutingModule {}
--- src/app/modules/admin/mentee/mentee.module.ts
+++ src/app/modules/admin/mentee/mentee.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MenteeRoutingModule } from './mentee-routing.module';
+import { MenteeHomeComponent } from './mentee-home/mentee-home.component';
+import { MenteeListComponent } from './mentee-list/mentee-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenteeHomeComponent,
+    MenteeListComponent
+  ],
   imports: [
     CommonModule,
     MenteeRoutingModule
