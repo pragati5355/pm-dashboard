f2b2aed61c434282341944e4e9057a055fa2d9f6
PRAGATI B GAWADE <pragati.gawade@mindbowser.com>
2023-11-20 15:10:38 +0530

    resolved the bug of filter in resource tab

--- src/app/modules/admin/resources/add-resources/add-resources.component.ts
+++ src/app/modules/admin/resources/add-resources/add-resources.component.ts
@@ -258,14 +258,15 @@ export class AddResourcesComponent implements OnInit, IDeactivateComponent {
     }
 
     removeTechnology(index: number, technologyControlValue: any) {
-        if (technologyControlValue?.id) {
-            const control = this.technologies?.at(index);
-            control?.get('experienceYear').setErrors(null);
-            control?.get('experienceMonth').setErrors(null);
-            control?.get('deleted')?.setValue(true);
-        } else {
-            this.technologies.removeAt(index);
-        }
+        this.technologies.removeAt(index);
+        // if (technologyControlValue?.id) {
+        //     const control = this.technologies?.at(index);
+        //     control?.get('experienceYear').setErrors(null);
+        //     control?.get('experienceMonth').setErrors(null);
+        //     control?.get('deleted')?.setValue(true);
+        // } else {
+        //     this.technologies.removeAt(index);
+        // }
     }
 
     addNewCertificate() {
@@ -420,7 +421,7 @@ export class AddResourcesComponent implements OnInit, IDeactivateComponent {
                 };
                 this.submitInProcess = true;
                 console.log("Payload : ", payload);
-                this.updateReourceApi(payload);
+                // this.updateReourceApi(payload);
             } else {
                 this.submitInProcess = false;
                 this.snackBar.errorSnackBar('Choose technology');
--- src/app/modules/admin/resources/resources-list/resources-list.component.ts
+++ src/app/modules/admin/resources/resources-list/resources-list.component.ts
@@ -247,6 +247,7 @@ export class ResourcesListComponent implements OnInit {
     }
 
     clearFilter() {
+        console.log("clear filter");
         this.isBench = false;
         this.isShadow = false;
         this.showVendorsOnly = false;
@@ -259,6 +260,8 @@ export class ResourcesListComponent implements OnInit {
         this.searchValue = '';
         this.count = 1;
         this.totalPerPageData = 10;
+        this.totalPerPageData = 10;
+        this.projects?.enable();
         this.projects?.setValue('');
         this.showTechnologies = [];
         this.technologys.setValue('');
@@ -295,16 +298,28 @@ export class ResourcesListComponent implements OnInit {
     }
 
     showOnlyVendors(event: MatCheckboxChange) {
-        this.showVendorsOnly = event?.checked;
-        this.projects?.disable();
-        this.technologyCtrl?.disable();
-        this.isShadowIsBench?.disable();
-        this.exprienceForm?.disable();
-        this.count = 1;
-        this.pagination = false;
-        const payload = this.getDefaultSearchPayload();
-        this.initialLoading = true;
-        this.loadDataWithFilterPayload(payload);
+        if(event?.checked){
+            this.showVendorsOnly = event?.checked;
+            this.projects?.disable();
+            this.technologyCtrl?.disable();
+            this.isShadowIsBench?.disable();
+            this.exprienceForm?.disable();
+            this.count = 1;
+            this.pagination = false;
+            this.initialLoading = true;
+            this.technologys.disable();
+            const payload = this.getDefaultSearchPayload();
+            this.loadDataWithFilterPayload(payload);
+        } else {
+            this.exprienceForm.enable();
+            this.count = 1;
+            this.totalPerPageData = 10;
+            this.projects?.enable();
+            this.isShadowIsBench.enable();
+            this.pagination = false;
+            this.technologyCtrl?.enable();
+            this.getList();
+        }
     }
 
     handleScroll() {
