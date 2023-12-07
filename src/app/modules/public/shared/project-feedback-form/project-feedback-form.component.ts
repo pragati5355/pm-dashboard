16d473ba12dc3b6edfce296c36c813ab6c88ae8d
Pragati B Gawade <pragati.gawade@mindbowser.com>
2023-12-07 16:38:26 +0530

    added the resource dynamically in the form

--- src/app/modules/public/shared/project-feedback-form/project-feedback-form.component.ts
+++ src/app/modules/public/shared/project-feedback-form/project-feedback-form.component.ts
@@ -4,63 +4,73 @@ import { AddFormService } from '@services/add-form.service';
 import { SnackBar } from 'app/core/utils/snackBar';
 
 @Component({
-  selector: 'app-project-feedback-form',
-  templateUrl: './project-feedback-form.component.html',
-  styleUrls: ['./project-feedback-form.component.scss']
+    selector: 'app-project-feedback-form',
+    templateUrl: './project-feedback-form.component.html',
+    styleUrls: ['./project-feedback-form.component.scss'],
 })
 export class ProjectFeedbackFormComponent implements OnInit {
+    public form!: any;
+    projectResources: any = [];
+    routeSubscribe: any;
+    initialLoading = false;
+    projectName = '';
+    projectId: any;
+    email: any;
 
-  public form!: Object;
-  formData :any = [];
-  routeSubscribe: any;
-  initialLoading= false
-  projectName = ""
-  projectId: any
-  sprintId: any
-  email: any
+    constructor(
+        private snackBar: SnackBar,
+        private formService: AddFormService,
+        private _route: ActivatedRoute,
+        private router: Router
+    ) {}
 
-  constructor(
-    private snackBar: SnackBar,
-    private formService: AddFormService,
-    private _route: ActivatedRoute,
-    private router: Router, 
-  ) { }
+    ngOnInit(): void {
+        this.routeSubscribe = this._route.queryParams.subscribe((params) => {
+            this.email = params['email'];
+        });
+        this.routeSubscribe = this._route.params.subscribe((res: any) => {
+            if (res) {
+                this.projectId = res['projectId'];
+                this.getFormDetails(this.projectId);
+            }
+        });
+    }
 
-  ngOnInit(): void {
-    this.routeSubscribe = this._route.queryParams.subscribe(
-      (params)=>{
-        this.email = params['email']
-    })
-    this.routeSubscribe = this._route.params.subscribe(
-      (res:any)=> {
-        if(res){
-          this.projectId = res['projectId']
-          console.log("projectId : ", this.projectId);
-          this.getFormDetails(this.projectId);
+    getFormDetails(id: any) {
+        this.initialLoading = true;
+        this.formService.getProjectFeedbackFormDetails(id).subscribe(
+            (res: any) => {
+                this.initialLoading = false;
+                if (!res?.error) {
+                    this.projectName = res?.data?.projectName;
+                    this.projectResources = res?.data?.resource;
+                    this.form = res?.data?.form?.formComponent;
+                    this.assignResourcesDynamically();
+                }
+            },
+            (err: any) => {
+                this.router.navigate([`/client-portal/empty-feedback-form`]);
+            }
+        );
+    }
+
+    assignResourcesDynamically() {
+      this.form?.components.forEach((item) => {
+        if (item?.label === 'Survey') {
+          item.questions =  this.projectResources;
         }
       });
-  }
+    }
 
-  getFormDetails(id : any){
-    this.initialLoading = true;
-    // this.formService.getProjectFeedbackFormDetails(id).subscribe(
-    //   (res:any)=> {
-    //     this.initialLoading = false;
-    //     if(!res?.error){
-    //       this.formData = res?.data;
-    //       this.projectName = 'Metrics';
-    //       console.log("this.formData : ", this.formData);
-    //     }
-    //   },
-    //   (err:any)=> {
-    //     this.router.navigate([`/client-portal/empty-feedback-form`]);
-    //   }
-    // )
-  }
+    submit(event: any) {
+      let formResponse = event.data;
+      let payload = {
+        formResponse: formResponse,
+        formComponent : this.form,
+        projectId: this.projectId,
+        emailId: this.email
+      }
+      console.log("Payload : ", payload);
 
-  submit(eventData : any){
-
-
-  }
-
+    }
 }
